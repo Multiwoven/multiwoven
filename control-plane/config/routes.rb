7e5a7e003b534c96f2d61c58918dd3443fa8f388
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2023-10-21 12:52:31 +0530

    Initialized Docker compose & Rails Docker setup. Added User, Workspace migrations & Models

--- control-plane/config/routes.rb
+++ control-plane/config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health Check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
+  # API routes
+  namespace :api do
+    namespace :v1 do
+      # Authentication Routes
+      scope :auth do
+        post 'signup', to: 'auth#signup'
+        post 'verify_code', to: 'auth#verify_code'
+        post 'login', to: 'auth#login'
+        delete 'logout', to: 'auth#logout'
+        post 'forgot_password', to: 'auth#forgot_password'
+        post 'reset_password', to: 'auth#reset_password'
+      end
+
+      # Workspace Routes
+      resources :workspaces
+    end
+  end
+
+  # Uncomment below if you have a root path
   # root "posts#index"
 end
--- control-plane/db/schema.rb
+++ control-plane/db/schema.rb
@@ -10,8 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_20_122228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti"
+    t.string "confirmation_code"
+    t.datetime "confirmed_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "workspaces", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+    t.string "status"
+    t.string "api_key"
+    t.string "workspace_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_workspaces_on_name", unique: true
+    t.index ["slug"], name: "index_workspaces_on_slug", unique: true
+    t.index ["workspace_id"], name: "index_workspaces_on_workspace_id", unique: true
+  end
 
 end
