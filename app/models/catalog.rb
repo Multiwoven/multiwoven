ce7a32e6f8dc941884587c57cca9ae099c57f993
Subin T P <subinthattaparambil@gmail.com>
2023-10-23 19:22:52 +0530

    Add comment on catalog

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -7,6 +7,5 @@ class Catalog < ApplicationRecord
   validates :workspace_id, presence: true
   validates :connector_id, presence: true
   validates :catalog, presence: true
-  # 32-bit Murmur3 hash
-  validates :catalog_hash, presence: true
+  validates :catalog_hash, presence: true # 32-bit Murmur3 hash
 end
