12b0ca77983ebb5c9e7ef0513e32a1b19b73e1b3
blade365z <amitabhyo@gmail.com>
2023-12-16 14:45:59 +0530

    tailwind config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const App = (): JSX.Element => {
   return <div>App</div>;
 };
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
