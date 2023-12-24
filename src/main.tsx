d67f8ad6f432949b9707fea7b0c74035dfc37f68
amitabhRazorpay <amitabh.baruah@razorpay.com>
2023-12-24 14:46:03 +0530

    removed tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./main.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
