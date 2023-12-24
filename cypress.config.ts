b37d6a96f2114be10469a49f8a14cfe105b85eca
vatan588 <vatan.deep588@gmail.com>
2023-12-25 04:27:38 +0530

    base url changes in cypree config as per workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: 'http://localhost:8000',
     specPattern: './cypress/e2e/**/*.cy.{js,jsx,ts,tsx,spec}',
     supportFile: false
   },
