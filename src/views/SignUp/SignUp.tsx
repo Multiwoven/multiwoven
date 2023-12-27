e0981bb63a2223d281809666ab4ad2222be82406
Tushar Selvakumar <tusharkumar91111@gmail.com>
2023-12-27 13:16:16 +0530

    Initial Commit

--- src/views/SignUp/SignUp.tsx
+++ src/views/SignUp/SignUp.tsx
@@ -1,6 +1,10 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import { 
+    Box, Button, FormControl, FormLabel, FormErrorMessage, Input, 
+    VStack, Image, Heading, Text, Link
+} from '@chakra-ui/react';
 import MultiwovenLogo from '../../assets/images/multiwoven-logo.png';
 
 // Yup validation schema
@@ -12,105 +16,72 @@ const SignUpSchema = Yup.object().shape({
         .min(8, 'Password must be at least 8 characters')
         .required('Password is required'),
     confirmPassword: Yup.string()
-        .oneOf([Yup.ref('password'), ''], 'Passwords must match')
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
         .required('Confirm Password is required'),
 });
 
 function SignUp() {
     return (
-        <>
-            <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        className="mx-auto h-10 w-auto"
-                        src={MultiwovenLogo}
-                        alt="Multiwoven"
-                    />
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign up for an account
-                    </h2>
-                </div>
+        <Box className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <Box className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <Image
+                    mx="auto"
+                    h="10"
+                    w="auto"
+                    src={MultiwovenLogo}
+                    alt="Multiwoven"
+                />
+                <Heading as="h2" mt="10" textAlign="center" size="xl">
+                    Sign up for an account
+                </Heading>
+            </Box>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-                    <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-                        <Formik
-                            initialValues={{ email: '', password: '', confirmPassword: '' }}
-                            validationSchema={SignUpSchema}
-                            onSubmit={(values, actions) => {
-                                console.log(values);
-                                actions.setSubmitting(false);
-                            }}
-                        >
-                            <Form className="space-y-6">
-                                <div>
-                                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                        Email address
-                                    </label>
-                                    <div className="mt-2">
-                                        <Field
-                                            id="email"
-                                            name="email"
-                                            type="email"
-                                            autoComplete="email"
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
-                                        />
-                                        <ErrorMessage name="email" component="div" className="text-red-500" />
-                                    </div>
-                                </div>
+            <Box mt="10" className="sm:mx-auto sm:w-full sm:max-w-[480px]">
+                <Box bg="white" px="6" py="12" shadow="sm" rounded="lg" className="sm:px-12">
+                    <Formik
+                        initialValues={{ email: '', password: '', confirmPassword: '' }}
+                        validationSchema={SignUpSchema}
+                        onSubmit={(values, actions) => {
+                            console.log(values);
+                            actions.setSubmitting(false);
+                        }}
+                    >
+                        {({ getFieldProps, errors, touched }) => (
+                            <Form>
+                                <FormControl id="email" isInvalid={errors.email && touched.email}>
+                                    <FormLabel>Email address</FormLabel>
+                                    <Input type="email" {...getFieldProps('email')} />
+                                    <FormErrorMessage>{errors.email}</FormErrorMessage>
+                                </FormControl>
 
-                                <div>
-                                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                        Password
-                                    </label>
-                                    <div className="mt-2">
-                                        <Field
-                                            id="password"
-                                            name="password"
-                                            type="password"
-                                            autoComplete="current-password"
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
-                                        />
-                                        <ErrorMessage name="password" component="div" className="text-red-500" />
-                                    </div>
-                                </div>
+                                <FormControl id="password" isInvalid={errors.password && touched.password}>
+                                    <FormLabel>Password</FormLabel>
+                                    <Input type="password" {...getFieldProps('password')} />
+                                    <FormErrorMessage>{errors.password}</FormErrorMessage>
+                                </FormControl>
 
-                                <div>
-                                    <label htmlFor="confirmPassword" className="block text-sm font-medium leading-6 text-gray-900">
-                                        Confirm Password
-                                    </label>
-                                    <div className="mt-2">
-                                        <Field
-                                            id="confirmPassword"
-                                            name="confirmPassword"
-                                            type="password"
-                                            autoComplete="current-password"
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
-                                        />
-                                        <ErrorMessage name="confirmPassword" component="div" className="text-red-500" />
-                                    </div>
-                                </div>
+                                <FormControl id="confirmPassword" isInvalid={errors.confirmPassword && touched.confirmPassword}>
+                                    <FormLabel>Confirm Password</FormLabel>
+                                    <Input type="password" {...getFieldProps('confirmPassword')} />
+                                    <FormErrorMessage>{errors.confirmPassword}</FormErrorMessage>
+                                </FormControl>
 
-                                <div>
-                                    <button
-                                        type="submit"
-                                        className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-                                    >
-                                        Sign Up!
-                                    </button>
-                                </div>
+                                <Button type="submit" colorScheme="orange" width="full">
+                                    Sign Up!
+                                </Button>
                             </Form>
-                        </Formik>
-                    </div>
+                        )}
+                    </Formik>
+                </Box>
 
-                    <p className="mt-10 text-center text-sm text-gray-500">
-                        Already have an account?{' '}
-                        <Link to="/login" className="font-semibold leading-6 text-orange-600 hover:text-orange-500">
-                            Sign In
-                        </Link>
-                    </p>
-                </div>
-            </div>
-        </>
+                <Text mt="10" textAlign="center" fontSize="sm" color="gray.500">
+                    Already have an account?{' '}
+                    <Link as={RouterLink} to="/login" color="orange.600" _hover={{ color: 'orange.500' }}>
+                        Sign In
+                    </Link>
+                </Text>
+            </Box>
+        </Box>
     )
 }
 
