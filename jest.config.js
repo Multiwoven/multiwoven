974b62a3663498887881e8c49396f59a11651d85
Amitabh <108728818+amitabh-blade@users.noreply.github.com>
2024-01-02 20:50:58 +0530

    Jest Configuration (#4)
    * added jest config for jsx
    
    * minor refactors

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    // process `*.tsx` files with `ts-jest`
+  },
+};
