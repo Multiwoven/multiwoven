1eeebc3146a4efcd203dcf73e87aede02fc4237e
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-01-05 20:33:46 +0530

    Added missed colors

--- src/chakra.config.ts
+++ src/chakra.config.ts
@@ -5,6 +5,7 @@ export const theme = extendTheme({
       mw_orange: "#E63D2D",
       secondary: "#731447",
       nav_bg: "#2d3748",
+      nav_text:"#a0aec0",
       primary:"#EB524C",
       hyperlink:"#5383EC",
       black:"#171923",
--- src/views/Sidebar/Sidebar.tsx
+++ src/views/Sidebar/Sidebar.tsx
@@ -39,14 +39,14 @@ const Sidebar = () => {
     // </SidebarContainer>
     <>
       <Container w='256px' display='flex' margin={0} padding={0} flexDir='row' className='flex flex-col align-center justify-center'>
-        <Box position={'relative'} padding={4} width={'100%'} bg='#171923' minH={'100vh'} color='black' borderRight={'1px'} borderRightColor={'border'}>
+        <Box position={'relative'} padding={4} width={'100%'} bg='black' minH={'100vh'} color='black' borderRight={'1px'} borderRightColor={'border'}>
           <Button padding={3} background={'nav_bg'} pt={6} pb={6} position={'relative'} w={'100%'} _hover={{ background: 'nav_bg', color: 'white' }} onClick={() => handleWorkPlace()}>
             <img width={25} src={IconImage} />
             <Box padding={2} width={'100%'}>
               <Text color={'white'} fontSize={13} textAlign="left" mt={0} w={'100%'}>Multiwoven</Text>
-              <Text color={'#a0aec0'} fontSize={11} textAlign="left" fontWeight='normal' mt={0} w={'100%'}>ID 345678</Text>
+              <Text color={'nav_text'} fontSize={11} textAlign="left" fontWeight='normal' mt={0} w={'100%'}>ID 345678</Text>
             </Box>
-            <UpDownIcon color={'#a0aec0'} fontSize={13} />
+            <UpDownIcon color={'nav_text'} fontSize={13} />
             {logoutPop && <Box fontWeight={'normal'} borderRadius={6} background={'white'} color={'nav_bg'} position={'absolute'} top={'55px'} left={0} padding={4} pt={5} pb={5} border={'1px'} borderColor={'border'} width={'100%'}>
               <Text fontSize={14} textAlign="left" mt={0} w={'100%'}>multiwoven@gmail.com</Text>
               <Text fontSize={14} textAlign="left" mt={5} w={'100%'}>Workplace setting</Text>
@@ -55,32 +55,32 @@ const Sidebar = () => {
             </Box>}
           </Button>
           <Text mt={6} w={'100%'}>
-            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/" background={location.pathname === '/' ? 'nav_bg' : ''} color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={HomeIcon} color={'#a0aec0'} fontSize={18} mr={2} /> Get Started</Link>
+            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/" background={location.pathname === '/' ? 'nav_bg' : ''} color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={HomeIcon} color={'nav_text'} fontSize={18} mr={2} /> Get Started</Link>
           </Text>
           <Text mt={6} w={'100%'}>
             <Text display='flex' pl={4} mb={2} textAlign="left" fontSize="12px" fontWeight={600} letterSpacing={2} color="gray.500">
               SETUP </Text>
-            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={CircleStackIcon} color={'#a0aec0'}  fontSize={18}  mr={2} /> Sources</Link>
-            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}> <Icon as={TableCellsIcon} color={'#a0aec0'}  fontSize={18}  mr={2} /> Destination</Link>
+            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={CircleStackIcon} color={'nav_text'}  fontSize={18}  mr={2} /> Sources</Link>
+            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}> <Icon as={TableCellsIcon} color={'nav_text'}  fontSize={18}  mr={2} /> Destination</Link>
             </Text>
           <Text mt={6} w={'100%'}>
             <Text display='flex' pl={4} mb={2} textAlign="left" fontSize="12px" fontWeight={600} letterSpacing={2} color="gray.500">
               DEFINE </Text>
             <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4}
              as={RouterLink} to="/models"
-              color="white" fontSize={14} background={location.pathname === '/models' ? 'nav_bg' : ''}  _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={ChartBarSquareIcon} color={'#a0aec0'}  fontSize={18} mr={2} /> Models</Link>
+              color="white" fontSize={14} background={location.pathname === '/models' ? 'nav_bg' : ''}  _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={ChartBarSquareIcon} color={'nav_text'}  fontSize={18} mr={2} /> Models</Link>
           </Text>
           <Text mt={6} w={'100%'}>
             <Text display='flex' pl={4} mb={2} textAlign="left" fontSize="12px" fontWeight={600} letterSpacing={2} color="gray.500">
               ACTIVATE </Text>
-            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={ArrowPathIcon} color={'#a0aec0'}  fontSize={18} mr={2} /> Syncs</Link>
-            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}> <Icon as={UserGroupIcon} color={'#a0aec0'}  fontSize={18} mr={2} /> Audiences</Link>
+            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={ArrowPathIcon} color={'nav_text'}  fontSize={18} mr={2} /> Syncs</Link>
+            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}> <Icon as={UserGroupIcon} color={'nav_text'}  fontSize={18} mr={2} /> Audiences</Link>
           </Text>
 
 
           <Text mt={6} w={'100%'} position={'absolute'} bottom={'60px'}>
-            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={CogIcon} color={'#a0aec0'}  fontSize={18} mr={2} /> Settings</Link>
-            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}> <Icon as={BookOpenIcon} color={'#a0aec0'}  fontSize={18} mr={2} /> Documentation</Link>
+            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}><Icon as={CogIcon} color={'nav_text'}  fontSize={18} mr={2} /> Settings</Link>
+            <Link fontWeight={500} mb={1} borderRadius={10} display={'flex'} alignItems={'center'} width={'100%'} padding={2} pl={4} as={RouterLink} to="/login" color="white" fontSize={14} _hover={{ background: 'nav_bg', color: 'white' }}> <Icon as={BookOpenIcon} color={'nav_text'}  fontSize={18} mr={2} /> Documentation</Link>
           </Text>
 
         </Box>
