0cc41d1163f3dc20f0e619760eac128f8eaa0540
vatan588 <vatan.deep588@gmail.com>
2024-01-12 00:19:00 +0530

    model comp. optimize

--- src/views/Models/Models.tsx
+++ src/views/Models/Models.tsx
@@ -1,32 +1,16 @@
 import {
     Box,
-    Button,
     Container,
     Stack,
-    Text,
-    Heading
 } from '@chakra-ui/react'
 import ModdelTable from "./table";
+import TopBar from './topBar';
 
 const Models = (): JSX.Element => {
 
     return (
         <Container minW={'100%'}>
-            <Box bgColor={'transparent'} as="section" pt={{ base: '4', md: '10' }} pb={{ base: '12', md: '12' }}>
-                <Stack spacing="4" direction={{ base: 'column', md: 'row' }} justify="space-between">
-                    <Stack spacing="1">
-                        <Heading size={{ base: 'xs', md: 'sm' }} fontWeight="medium">
-                            Member overview
-                        </Heading>
-                        <Text color="fg.muted">All registered users in the overview</Text>
-                    </Stack>
-                    <Stack direction="row" spacing="3">
-                        <Button variant="secondary">Invite</Button>
-                        <Button>Create</Button>
-                    </Stack>
-                </Stack>
-
-            </Box>
+            <TopBar />
             <Box
                 bg="bg.surface"
                 boxShadow={{ base: 'none', md: 'sm' }}
