e827ea20f4af24765095c120281907ca33ad9f7b
vatan588 <vatan.deep588@gmail.com>
2024-01-12 00:05:14 +0530

    optimize component

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import { MAIN_PAGE_ROUTES, AUTH_ROUTES } from "./routes";
 import Heading from "./components/Heading";
 import MainLayout from "./views/MainLayout";
+
 const App = () => {
   return (
     <div className="md:container md:mx-auto">
       <Routes>
-        {AUTH_ROUTES.map((authRoute) => (
-          <Route
-            path={authRoute.url}
-            element={authRoute.component}
-            key={authRoute.name}
-          />
+        {AUTH_ROUTES.map(({ url, component, name }) => (
+          <Route path={url} element={component} key={name} />
         ))}
+        
         <Route path="/" element={<MainLayout />}>
-          {MAIN_PAGE_ROUTES.map((pageRoute) => (
-            <Route
-              path={pageRoute.url}
-              element={pageRoute.component}
-              key={pageRoute.name}
-            />
+          {MAIN_PAGE_ROUTES.map(({ url, component, name }) => (
+            <Route path={url} element={component} key={name} />
           ))}
         </Route>
+        
         <Route
           path="*"
           element={<Heading size="small">Page Not Found</Heading>}
@@ -30,4 +25,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
