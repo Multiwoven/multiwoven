b68febbc4ec227f9d58415ff38799324614d1c44
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-01-21 13:15:41 +0530

    Added Chakra Pro Theme and Manrope Font, changed chakra.config to work with new theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from './chakra.config'
+import mwTheme from './chakra.config'
 import "./assets/styles/common_styles.scss"
  
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider theme={mwTheme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
