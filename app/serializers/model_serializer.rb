ab9c916be7a9359c3c182e0bc223f2cbd1722bf5
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2024-01-30 16:46:41 +0530

    Connector object response in models API & static assets serving (#40)
    * Connector object response in models API & static assets serving
    
    * Removed assets for serving it from GEM

--- app/serializers/model_serializer.rb
+++ app/serializers/model_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class ModelSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :query, :query_type, :primary_key, :connector_id, :created_at, :updated_at
+  attributes :id, :name, :description, :query, :query_type, :primary_key, :created_at, :updated_at
+
+  attribute :connector do
+    ConnectorSerializer.new(object.connector).attributes
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Health Check
   get "up" => "rails/health#show", as: :rails_health_check
-
+  
   # API routes
   namespace :api do
     namespace :v1 do
