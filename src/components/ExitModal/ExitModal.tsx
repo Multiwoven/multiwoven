da05139d64bfbf5bcb496aab9bddcf54e8e173dc
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-02-01 00:35:33 +0530

    Removed blur from Exit Modal

--- src/components/ExitModal/ExitModal.tsx
+++ src/components/ExitModal/ExitModal.tsx
@@ -26,7 +26,7 @@ const ExitModal = (): JSX.Element => {
       </Button>
 
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay bg="blackAlpha.400" backdropFilter="blur(2px)" />
+        <ModalOverlay bg="blackAlpha.400" />
         <ModalContent>
           <ModalCloseButton color="gray.300" />
           <ModalBody mx="auto" pt={10}>
