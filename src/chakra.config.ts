85748298c34622ad0038e1c26b07e1038af62ca2
blade365z <amitabhyo@gmail.com>
2024-01-31 18:14:52 +0530

    Revert "Merge branch 'main' of github.com:Multiwoven/multiwoven-ui into develop"
    This reverts commit 5a450e70ed3539fca0912354feafd4564e438c0f, reversing
    changes made to 3c2af0699ab1f1b8f2c9639f0e10273824eafbc0.

--- src/chakra.config.ts
+++ src/chakra.config.ts
@@ -5,23 +5,23 @@ import "@fontsource-variable/manrope";
 
 const proTheme = extendTheme(theme);
 const extenstion = {
-  colors: {
-    ...proTheme.colors,
-    brand: proTheme.colors.teal,
-    mw_orange: "#E63D2D",
-    secondary: "#731447",
-    nav_bg: "#2d3748",
-    nav_text: "#a0aec0",
-    primary: "#EB524C",
-    hyperlink: "#5383EC",
-    black: "#171923",
-    dark_gray: "#4b5563",
-    border: "#E2E8F0",
-  },
-  fonts: {
-    heading: "'Manrope', -apple-system, system-ui, sans-serif",
-    body: "'Manrope', -apple-system, system-ui, sans-serif",
-  },
+	colors: {
+		...proTheme.colors,
+		brand: proTheme.colors.teal,
+		mw_orange: "#E63D2D",
+		secondary: "#731447",
+		nav_bg: "#2d3748",
+		nav_text: "#a0aec0",
+		primary: "#EB524C",
+		hyperlink: "#5383EC",
+		black: "#171923",
+		dark_gray: "#4b5563",
+		border: "#E2E8F0",
+	},
+	fonts: {
+		heading: "'Manrope', -apple-system, system-ui, sans-serif",
+		body: "'Manrope', -apple-system, system-ui, sans-serif",
+	},
 };
 
 const mwTheme = extendTheme(extenstion, proTheme);
--- src/views/Sidebar/navButton.tsx
+++ src/views/Sidebar/navButton.tsx
@@ -1,20 +1,18 @@
-import { As, Button, ButtonProps, HStack, Icon, Text } from "@chakra-ui/react";
+import { As, Button, ButtonProps, HStack, Icon, Text } from '@chakra-ui/react'
 
 interface NavButtonProps extends ButtonProps {
-  icon: As;
-  label: string;
+  icon: As
+  label: string
 }
 
-export const NavButton = (props: NavButtonProps): JSX.Element => {
-  const { icon, label, ...buttonProps } = props;
+export const NavButton = (props: NavButtonProps) : JSX.Element => {
+  const { icon, label, ...buttonProps } = props
   return (
     <Button variant="tertiary" justifyContent="start" {...buttonProps}>
       <HStack spacing="2">
         <Icon as={icon} boxSize="4" color="fg.subtle" />
-        <Text color={"#101828"} fontWeight={"500"} fontSize="sm">
-          {label}
-        </Text>
+        <Text color={'#101828'} fontWeight={'500'} fontSize='sm'>{label}</Text>
       </HStack>
     </Button>
-  );
-};
+  )
+}
