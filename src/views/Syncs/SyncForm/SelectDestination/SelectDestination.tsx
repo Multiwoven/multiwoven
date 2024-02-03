2a01100a2051eba40ce3ce364c339e9657b842b0
blade365z <amitabhyo@gmail.com>
2024-02-04 00:52:09 +0530

    destination selection event added

--- src/views/Syncs/SyncForm/SelectDestination/SelectDestination.tsx
+++ src/views/Syncs/SyncForm/SelectDestination/SelectDestination.tsx
@@ -1,13 +1,21 @@
 import ContentContainer from "@/components/ContentContainer";
+import { SteppedFormContext } from "@/components/SteppedForm/SteppedForm";
 import DestinationsTable from "@/views/Connectors/Destinations/DestinationsList/DestinationsTable";
 import { Box } from "@chakra-ui/react";
+import { useContext } from "react";
 
 const SelectDestination = (): JSX.Element => {
+  const { stepInfo, handleMoveForward } = useContext(SteppedFormContext);
+
+  const handleOnRowClick = (data: unknown) => {
+    handleMoveForward(stepInfo?.formKey as string, data);
+  };
+
   return (
     <Box>
       <ContentContainer>
         <DestinationsTable
-          handleOnRowClick={(data) => console.log("Clicked: ", data)}
+          handleOnRowClick={(data) => handleOnRowClick(data)}
         />
       </ContentContainer>
     </Box>
