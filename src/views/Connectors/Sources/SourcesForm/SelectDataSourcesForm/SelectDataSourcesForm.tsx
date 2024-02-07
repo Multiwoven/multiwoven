a2c3b550fb587f036637c56cd2d26d52dc4f2dc0
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-02-07 16:24:25 +0530

    Added Icon to Select Data Source Form

--- src/views/Connectors/Sources/SourcesForm/SelectDataSourcesForm/SelectDataSourcesForm.tsx
+++ src/views/Connectors/Sources/SourcesForm/SelectDataSourcesForm/SelectDataSourcesForm.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import { SteppedFormContext } from "@/components/SteppedForm/SteppedForm";
 import { getConnectorsDefintions } from "@/services/connectors";
 import { useQuery } from "@tanstack/react-query";
@@ -45,26 +45,33 @@ const SelectDataSourcesForm = (): JSX.Element => {
             width="100%"
             borderRadius="8px"
             borderWidth="thin"
+            borderColor="gray.400"
             marginRight="10px"
             marginBottom="10px"
             cursor="pointer"
             display="flex"
             alignItems="center"
             onClick={() => handleOnClick(datasource)}
-            backgroundColor={
-              currentForm?.datasource === datasource.name ? "gray.100" : "white"
-            }
           >
             <Box>
-              <Image
-                src={datasource.icon}
-                height="8"
-                w="min"
-                mr={3}
-              />
+              <Image src={datasource.icon} height="8" w="min" mr={3} />
             </Box>
-            <Box>
-              <Text fontWeight={600}>{datasource.name}</Text>
+            <Box display="flex" alignItems="center">
+              <Box
+                height="40px"
+                width="40px"
+                marginRight="10px"
+                borderWidth="thin"
+                padding="5px"
+                borderRadius="8px"
+              >
+                <Image
+                  src={datasource.icon}
+                  alt="source icon"
+                  maxHeight="100%"
+                />
+              </Box>
+              <Text fontWeight='bold'>{datasource.name}</Text>
             </Box>
           </Box>
         ))}
