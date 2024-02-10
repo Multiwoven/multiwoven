fa223e92d20751f699501e4aa4e9201f74fd05d1
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-02-10 19:45:33 +0530

    Centered Loader and changed color

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,17 +1,26 @@
-import { Box, Spinner } from "@chakra-ui/react"
+import { Box, Center, Flex, Spinner } from "@chakra-ui/react";
 
-const Loader = () : JSX.Element => {
-    return(
-        <Box width="100%" display="flex" justifyContent="center">
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      </Box>
-    )
-}
+const Loader = (): JSX.Element => {
+  return (
+    <Flex
+      width="100%"
+      height="100vh"
+      alignContent="center"
+      justifyContent="center"
+    >
+      <Center>
+        <Box>
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="brand.500"
+            size="xl"
+          />
+        </Box>
+      </Center>
+    </Flex>
+  );
+};
 
 export default Loader;
