5af27af3e5aa981c0b24fa852eaaf7a4822e6210
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-02-10 12:16:08 +0530

    Fixed Test Errors

--- src/views/Authentication/SignIn/signin.test.tsx
+++ src/views/Authentication/SignIn/signin.test.tsx
@@ -1,42 +1,43 @@
-import { render, fireEvent, waitFor } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
-import { MemoryRouter } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import SignIn from './SignIn';
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Cookies from "js-cookie";
+import SignIn from "./SignIn";
 
-jest.mock('@/services/common', () => ({
-  login: jest.fn().mockResolvedValue({ success: true, response: { data: { token: 'mockToken' } } }),
+jest.mock("@/services/common", () => ({
+  login: jest.fn().mockResolvedValue({
+    success: true,
+    response: { data: { token: "mockToken" } },
+  }),
 }));
 
-describe('Login Component', () => {
-  it('renders login form and handles submit', async () => {
+describe("Login Component", () => {
+  it("renders login form and handles submit", async () => {
     const { getByText, getByPlaceholderText } = render(
       <MemoryRouter>
         <SignIn />
       </MemoryRouter>
     );
 
-    // dummy form values
-    const email = 'test@example.com';
-    const password = 'password123';
-
     // form fileds on change
-    act(() => {
-      fireEvent.change(getByPlaceholderText('Email'), { target: { value: email } });
-      fireEvent.change(getByPlaceholderText('********'), { target: { value: password } });
+    const email = "test@example.com";
+    const password = "password123";
+
+    fireEvent.change(getByPlaceholderText("Email"), {
+      target: { value: email },
+    });
+    fireEvent.change(getByPlaceholderText("Password"), {
+      target: { value: password },
     });
 
     // click on login
-    act(() => {
-      fireEvent.click(getByText('Sign in'));
-    });
+    fireEvent.click(getByText("Sign In"));
 
     // post api call
     await waitFor(() => {
       if (Cookies) {
-        const authTokenCookie = Cookies.get('authToken');
-        expect(authTokenCookie).toContain('authToken=mockToken')
-        expect(window.location.pathname).toBe('/');
+        const authTokenCookie = Cookies.get("authToken");
+        expect(authTokenCookie).toContain("authToken=mockToken");
+        expect(window.location.pathname).toBe("/");
       }
     });
   });
