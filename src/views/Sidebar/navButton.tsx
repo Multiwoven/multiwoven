8c0c219792679fc61c5f3925d42da5ad40e798cc
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-02-13 20:14:41 +0530

    fixed weaving soon badge

--- src/views/Sidebar/navButton.tsx
+++ src/views/Sidebar/navButton.tsx
@@ -51,13 +51,10 @@ export const NavButton = (props: NavButtonProps): JSX.Element => {
             borderRadius="4px"
             borderColor="gray.500"
             gap="10px"
+            px="2px"
+            display="flex"
           >
-            <Text
-              fontSize={10}
-              color="gray.600"
-              justifyContent="center"
-              alignSelf="center"
-            >
+            <Text fontSize="10px" fontWeight={500} color="gray.600">
               weaving soon
             </Text>
           </Box>
