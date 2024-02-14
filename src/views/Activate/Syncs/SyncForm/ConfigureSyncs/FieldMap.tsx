837318f1cae0f7753e3985a7681778555f704ab4
sumitd94 <sumitdhanania94@gmail.com>
2024-02-14 21:36:00 +0530

    style: update styles for step 3 in syncs page

--- src/views/Activate/Syncs/SyncForm/ConfigureSyncs/FieldMap.tsx
+++ src/views/Activate/Syncs/SyncForm/ConfigureSyncs/FieldMap.tsx
@@ -1,16 +1,16 @@
-import EntityItem from "@/components/EntityItem";
-import { Box, Select } from "@chakra-ui/react";
+import EntityItem from '@/components/EntityItem';
+import { Box, Select } from '@chakra-ui/react';
 
 type FieldMapProps = {
   id: number;
-  fieldType: "model" | "destination";
+  fieldType: 'model' | 'destination';
   icon: string;
   entityName: string;
   options: string[];
   disabledOptions?: string[];
   value: string;
   isDisabled: boolean;
-  onChange: (id: number, type: "model" | "destination", value: string) => void;
+  onChange: (id: number, type: 'model' | 'destination', value: string) => void;
 };
 
 const FieldMap = ({
@@ -25,18 +25,22 @@ const FieldMap = ({
   isDisabled,
 }: FieldMapProps): JSX.Element => {
   return (
-    <Box width="100%">
-      <Box marginBottom="10px">
+    <Box width='100%'>
+      <Box marginBottom='10px'>
         <EntityItem icon={icon} name={entityName} />
       </Box>
       <Box>
         <Select
           value={value}
           placeholder={`Select a field from ${entityName}`}
-          backgroundColor="#fff"
+          backgroundColor='#fff'
           isDisabled={isDisabled}
           onChange={(e) => onChange(id, fieldType, e.target.value)}
           isRequired
+          borderWidth='1px'
+          borderStyle='solid'
+          borderColor='gray.400'
+          color='gray.600'
         >
           {options.map((option) => (
             <option
