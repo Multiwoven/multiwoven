467999a8b81014b24ec5a1ad7271d50c1b085a4b
sumitd94 <sumitdhanania94@gmail.com>
2024-02-14 12:05:00 +0530

    updated colors and font weight for no syncs page

--- src/views/Activate/NoSyncs/NoSyncs.tsx
+++ src/views/Activate/NoSyncs/NoSyncs.tsx
@@ -7,10 +7,10 @@ import {
   Image,
   Text,
   VStack,
-} from "@chakra-ui/react";
-import { FiPlus } from "react-icons/fi";
-import NoSyncsImage from "@/assets/images/NoSyncs.png";
-import { useNavigate } from "react-router-dom";
+} from '@chakra-ui/react';
+import { FiPlus } from 'react-icons/fi';
+import NoSyncsImage from '@/assets/images/NoSyncs.png';
+import { useNavigate } from 'react-router-dom';
 
 type NoActivationsProps = {
   activationType: string;
@@ -19,36 +19,39 @@ type NoActivationsProps = {
 const NoActivations = ({ activationType }: NoActivationsProps): JSX.Element => {
   const navigate = useNavigate();
 
-  const type = activationType === "sync" ? "Sync" : "";
   const description =
-    activationType === "sync"
-      ? "Add a Sync to declare how you want query results from a Model to appear in your destination"
-      : "";
+    activationType === 'sync'
+      ? 'Add a Sync to declare how you want query results from a Model to appear in your destination'
+      : '';
 
-  const image = activationType === "sync" ? NoSyncsImage : "";
+  const image = activationType === 'sync' ? NoSyncsImage : '';
 
   return (
     <Flex
-      width="100%"
-      height="100vh"
-      alignContent="center"
-      justifyContent="center"
+      width='100%'
+      height='100vh'
+      alignContent='center'
+      justifyContent='center'
     >
       <Center>
-        <Box maxW="sm" textAlign="center">
+        <Box maxW='sm' textAlign='center'>
           <VStack spacing={8}>
             <VStack>
               <Image src={image} />
-              <Heading size="xs">No {type}s found</Heading>
-              <Text size="sm">{description} </Text>
+              <Heading size='xs' color='black.500' fontWeight={600}>
+                No {activationType}s found
+              </Heading>
+              <Text size='sm' color='black.200' fontWeight={400}>
+                {description}{' '}
+              </Text>
             </VStack>
             <Button
-              onClick={() => navigate("new")}
+              onClick={() => navigate('new')}
               leftIcon={<FiPlus />}
-              variant="solid"
-              w="fit"
+              variant='solid'
+              w='fit'
             >
-              Add {type}
+              Add {activationType}
             </Button>
           </VStack>
         </Box>
