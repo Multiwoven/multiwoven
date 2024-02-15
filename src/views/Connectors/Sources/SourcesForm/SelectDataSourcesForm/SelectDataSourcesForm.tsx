2b5f1c80f176645020c9727341a7bb3eabb49e7f
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-02-15 14:47:13 +0530

    fixed build errors

--- src/views/Connectors/Sources/SourcesForm/SelectDataSourcesForm/SelectDataSourcesForm.tsx
+++ src/views/Connectors/Sources/SourcesForm/SelectDataSourcesForm/SelectDataSourcesForm.tsx
@@ -4,12 +4,10 @@ import { SteppedFormContext } from "@/components/SteppedForm/SteppedForm";
 import { getConnectorsDefintions } from "@/services/connectors";
 import { useQuery } from "@tanstack/react-query";
 import { DatasourceType } from "@/views/Connectors/types";
-import { useUiConfig } from "@/utils/hooks";
 import ContentContainer from "@/components/ContentContainer";
 
 const SelectDataSourcesForm = (): JSX.Element => {
   const { stepInfo, handleMoveForward } = useContext(SteppedFormContext);
-  const { maxContentWidth } = useUiConfig();
 
   const { data } = useQuery({
     queryKey: ["datasources", "source"],
@@ -30,50 +28,50 @@ const SelectDataSourcesForm = (): JSX.Element => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
       <ContentContainer>
-      <Box
-        display={{ base: "block", md: "grid" }}
-        gridTemplateColumns="1fr 1fr"
-        gap="20px"
-        marginBottom="20px"
-        paddingY="10px"
-        width="100%"
-      >
-        {datasources.map((datasource) => (
-          <Box
-            key={datasource.name}
-            paddingX="12px"
-            paddingY="8px"
-            width="100%"
-            borderRadius="8px"
-            borderWidth="thin"
-            borderColor="gray.400"
-            marginRight="10px"
-            marginBottom="10px"
-            cursor="pointer"
-            display="flex"
-            alignItems="center"
-            onClick={() => handleOnClick(datasource)}
-          >
-            <Box display="flex" alignItems="center">
-              <Box
-                height="40px"
-                width="40px"
-                marginRight="10px"
-                borderWidth="thin"
-                padding="5px"
-                borderRadius="8px"
-              >
-                <Image
-                  src={datasource.icon}
-                  alt="source icon"
-                  maxHeight="100%"
-                />
+        <Box
+          display={{ base: "block", md: "grid" }}
+          gridTemplateColumns="1fr 1fr"
+          gap="20px"
+          marginBottom="20px"
+          paddingY="10px"
+          width="100%"
+        >
+          {datasources.map((datasource) => (
+            <Box
+              key={datasource.name}
+              paddingX="12px"
+              paddingY="8px"
+              width="100%"
+              borderRadius="8px"
+              borderWidth="thin"
+              borderColor="gray.400"
+              marginRight="10px"
+              marginBottom="10px"
+              cursor="pointer"
+              display="flex"
+              alignItems="center"
+              onClick={() => handleOnClick(datasource)}
+            >
+              <Box display="flex" alignItems="center">
+                <Box
+                  height="40px"
+                  width="40px"
+                  marginRight="10px"
+                  borderWidth="thin"
+                  padding="5px"
+                  borderRadius="8px"
+                >
+                  <Image
+                    src={datasource.icon}
+                    alt="source icon"
+                    maxHeight="100%"
+                  />
+                </Box>
+                <Text fontWeight="bold">{datasource.name}</Text>
               </Box>
-              <Text fontWeight='bold'>{datasource.name}</Text>
             </Box>
-          </Box>
-        ))}
-      </Box>
+          ))}
+        </Box>
       </ContentContainer>
     </Box>
   );
