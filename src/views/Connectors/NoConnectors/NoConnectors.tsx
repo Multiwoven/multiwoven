1296f7c122d6d64da6a110173383bdfa7d246707
sumitd94 <sumitdhanania94@gmail.com>
2024-02-16 16:36:39 +0530

    fix: redirect routes for adding models and connectors

--- src/views/Connectors/NoConnectors/NoConnectors.tsx
+++ src/views/Connectors/NoConnectors/NoConnectors.tsx
@@ -6,11 +6,11 @@ import {
   Image,
   Text,
   VStack,
-} from "@chakra-ui/react";
-import { FiPlus } from "react-icons/fi";
-import NoSourcesImage from "@/assets/images/NoSources.png";
-import NoDestinationsImage from "@/assets/images/NoDestinations.png";
-import { useNavigate } from "react-router-dom";
+} from '@chakra-ui/react';
+import { FiPlus } from 'react-icons/fi';
+import NoSourcesImage from '@/assets/images/NoSources.png';
+import NoDestinationsImage from '@/assets/images/NoDestinations.png';
+import { useNavigate } from 'react-router-dom';
 
 type NoConnectorsProps = {
   connectorType: string;
@@ -19,34 +19,36 @@ type NoConnectorsProps = {
 const NoConnectors = ({ connectorType }: NoConnectorsProps): JSX.Element => {
   const navigate = useNavigate();
 
-  const type = connectorType === "source" ? "Source" : "Destination";
+  const type = connectorType === 'source' ? 'Source' : 'Destination';
   const description =
-    connectorType === "source"
-      ? "Add a destination where your data will be sent"
-      : "Configure a source where your data is stored and managed";
+    connectorType === 'source'
+      ? 'Add a destination where your data will be sent'
+      : 'Configure a source where your data is stored and managed';
 
   const image =
-    connectorType === "source" ? NoSourcesImage : NoDestinationsImage;
+    connectorType === 'source' ? NoSourcesImage : NoDestinationsImage;
 
   return (
     <Flex
-      width="100%"
-      height="100vh"
-      alignContent="center"
-      justifyContent="center"
+      width='100%'
+      height='100vh'
+      alignContent='center'
+      justifyContent='center'
     >
       <Center>
         <VStack spacing={8}>
           <VStack>
             <Image src={image} />
-            <Heading size="xs">No {type}s added</Heading>
-            <Text size="sm">{description} </Text>
+            <Heading size='xs'>No {type}s added</Heading>
+            <Text size='sm'>{description} </Text>
           </VStack>
           <Button
-            onClick={() => navigate("new")}
+            onClick={() =>
+              navigate('/setup/destinations/new', { replace: true })
+            }
             leftIcon={<FiPlus />}
-            variant="solid"
-            w="fit"
+            variant='solid'
+            w='fit'
           >
             Add {type}
           </Button>
