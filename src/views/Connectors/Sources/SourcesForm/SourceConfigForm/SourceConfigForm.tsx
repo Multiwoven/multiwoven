6c298faba7ac80035e9f5668a0fe5495b207ca9b
Allen Francis <lallenfrancisl@gmail.com>
2024-02-16 12:14:16 +0530

    refactor: remove unwanted code added for debugging

--- src/views/Connectors/Sources/SourcesForm/SourceConfigForm/SourceConfigForm.tsx
+++ src/views/Connectors/Sources/SourcesForm/SourceConfigForm/SourceConfigForm.tsx
@@ -46,25 +46,15 @@ const SourceConfigForm = (): JSX.Element | null => {
     <Box display="flex" justifyContent="center" marginBottom="80px">
       <ContentContainer>
         <Box backgroundColor="gray.200" padding="20px" borderRadius="8px">
-          {(connectorSchema as any).title.toLowerCase() === "amazon redshift"
-            ? <Form
-                uiSchema={uiSchema}
-                schema={schema as any}
-                validator={validator}
-                templates={{ObjectFieldTemplate: MWObjectFieldTemplate, TitleFieldTemplate: MWTitleField}}
-                onSubmit={({ formData }) => handleFormSubmit(formData)}
-              >
-                <SourceFormFooter ctaName="Continue" ctaType="submit" />
-              </Form>
-            : <Form
-                schema={connectorSchema}
-                validator={validator}
-                templates={{ObjectFieldTemplate: MWObjectFieldTemplate}}
-                onSubmit={({ formData }) => handleFormSubmit(formData)}
-              >
-                <SourceFormFooter ctaName="Continue" ctaType="submit" />
-              </Form>
-          }
+          <Form
+            uiSchema={(connectorSchema as any).title.toLowerCase() === "amazon redshift" ? uiSchema : undefined}
+            schema={connectorSchema}
+            validator={validator}
+            templates={{ObjectFieldTemplate: MWObjectFieldTemplate, TitleFieldTemplate: MWTitleField}}
+            onSubmit={({ formData }) => handleFormSubmit(formData)}
+          >
+            <SourceFormFooter ctaName="Continue" ctaType="submit" />
+          </Form>
         </Box>
       </ContentContainer>
     </Box>
@@ -91,77 +81,6 @@ const uiSchema = {
       "ui:widget": "hidden"
     }
   }
-}
-
-const schema = {
-  $schema: "http://json-schema.org/draft-07/schema#",
-  title: "Amazon Redshift",
-  type: "object",
-  required: ["host", "port", "database", "schema"],
-  properties: {
-    credentials: {
-      title: "",
-      type: "object",
-      required: ["auth_type", "username", "password"],
-      properties: {
-        auth_type: {
-          type: "string",
-          default: "username/password",
-          order: 0,
-          readOnly: true,
-        },
-        username: {
-          description:
-            "Username refers to your individual Redshift login credentials. At a minimum, the user associated with these credentials must be granted read access to the data intended for synchronization.",
-          examples: ["REDSHIFT_USER"],
-          type: "string",
-          title: "Username",
-          order: 1,
-        },
-        password: {
-          description:
-            "This field requires the password associated with the user account specified in the preceding section.",
-          type: "string",
-          multiwoven_secret: true,
-          title: "Password",
-          order: 2,
-        },
-      },
-      order: 0,
-    },
-    host: {
-      description:
-        "The hostname or IP address of your Redshift cluster represents a critical connectivity parameter. To retrieve this information, access the Redshift web console, proceed to the Clusters panel, and select your specific cluster. Within the cluster's details, locate and copy the Endpoint string, ensuring to omit the port number and database name appended at the conclusion.",
-      examples: [
-        "example-redshift-cluster.abcdefg.us-west-2.redshift.amazonaws.com",
-      ],
-      type: "string",
-      title: "Host",
-      order: 1,
-    },
-    port: {
-      description:
-        "The port number for your Redshift cluster, which defaults to 5439, may vary based on your configuration. To verify the specific port number assigned to your cluster, access the Redshift web console, proceed to the Clusters panel, and select your cluster. You can find the port number displayed within the Properties tab.",
-      examples: ["5439"],
-      type: "string",
-      title: "Port",
-      order: 2,
-    },
-    database: {
-      description: "The specific Redshift database to connect to.",
-      examples: ["REDSHIFT_DB"],
-      type: "string",
-      title: "Database",
-      order: 3,
-    },
-    schema: {
-      description: "The schema within the Redshift database.",
-      examples: ["REDSHIFT_SCHEMA"],
-      type: "string",
-      title: "Schema",
-      order: 4,
-    },
-  },
 }
 
 export default SourceConfigForm;
