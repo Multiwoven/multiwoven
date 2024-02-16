59de8b17f3fa2641362b308bb60d2638fa936c71
Allen Francis <lallenfrancisl@gmail.com>
2024-02-16 11:06:59 +0530

    feat: add support for specifying colspans

--- src/views/Connectors/Sources/SourcesForm/SourceConfigForm/MWObjectFieldTemplate.tsx
+++ src/views/Connectors/Sources/SourcesForm/SourceConfigForm/MWObjectFieldTemplate.tsx
@@ -1,6 +1,6 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem } from "@chakra-ui/react"
 import {
-//   canExpand,
+  canExpand,
   descriptionId,
   FormContextType,
   getTemplate,
@@ -9,52 +9,56 @@ import {
   RJSFSchema,
   StrictRJSFSchema,
   titleId,
-} from '@rjsf/utils';
-import DefaultObjectFieldTemplate from "@rjsf/chakra-ui/src/ObjectFieldTemplate";
+} from "@rjsf/utils"
+import DefaultObjectFieldTemplate from "@rjsf/chakra-ui/src/ObjectFieldTemplate"
 
 export default function ObjectFieldTemplate<
   T = any,
   S extends StrictRJSFSchema = RJSFSchema,
-  F extends FormContextType = any
+  F extends FormContextType = any,
 >(props: ObjectFieldTemplateProps<T, S, F>) {
   const {
     description,
     title,
     properties,
     required,
-    // disabled,
-    // readonly,
+    disabled,
+    readonly,
     uiSchema,
     idSchema,
     schema,
-    // formData,
-    // onAddClick,
+    formData,
+    onAddClick,
     registry,
-  } = props;
-  const uiOptions = getUiOptions<T, S, F>(uiSchema);
-  const TitleFieldTemplate = getTemplate<'TitleFieldTemplate', T, S, F>('TitleFieldTemplate', registry, uiOptions);
-  const DescriptionFieldTemplate = getTemplate<'DescriptionFieldTemplate', T, S, F>(
-    'DescriptionFieldTemplate',
+  } = props
+
+  const uiOptions = getUiOptions<T, S, F>(uiSchema)
+  const TitleFieldTemplate = getTemplate<"TitleFieldTemplate", T, S, F>(
+    "TitleFieldTemplate",
     registry,
     uiOptions
-  );
-  
-  const layout = uiOptions.layout as Record<string, string>;
+  )
+  const DescriptionFieldTemplate = getTemplate<
+    "DescriptionFieldTemplate",
+    T,
+    S,
+    F
+  >("DescriptionFieldTemplate", registry, uiOptions)
 
-  if (layout?.display !== "grid" || !layout?.cols) {
-    return <DefaultObjectFieldTemplate {...props}  />
+  const layout = uiOptions.layout as Record<string, string>
+
+  if (layout?.display !== "grid" || !layout?.cols || !Array.isArray(layout?.colspans)) {
+    return <DefaultObjectFieldTemplate {...props} />
   }
 
   // Button templates are not overridden in the uiSchema
-//   const {
-//     ButtonTemplates: { AddButton },
-//   } = registry.templates;
-  const gridTemplate = `repeat(${layout.cols}, 1fr)`;
+  const {
+    ButtonTemplates: { AddButton },
+  } = registry.templates
+  const gridTemplate = `repeat(${layout.cols}, 1fr)`
 
   return (
     <>
-        Custom template
-
       {title && (
         <TitleFieldTemplate
           id={titleId<T>(idSchema)}
@@ -74,37 +78,28 @@ export default function ObjectFieldTemplate<
           registry={registry}
         />
       )}
-      
-    <Grid templateColumns={gridTemplate} gap={6} >
+      <Grid templateColumns={gridTemplate} gap={6}>
         {properties.map((element, index) =>
           element.hidden ? (
             element.content
           ) : (
-            <GridItem key={`${idSchema.$id}-${element.name}-${index}`}>{element.content}</GridItem>
+            <GridItem key={`${idSchema.$id}-${element.name}-${index}`} colSpan={Number(layout.colspans[index])}>
+              {element.content}
+            </GridItem>
           )
         )}
-    </Grid>
-      
-      {/* <Grid gap={description ? 2 : 6} mb={4}>
-        {properties.map((element, index) =>
-          element.hidden ? (
-            element.content
-          ) : (
-            <GridItem key={`${idSchema.$id}-${element.name}-${index}`}>{element.content}</GridItem>
-          )
-        )}
-        {canExpand<T, S, F>(schema, uiSchema, formData) && (
-          <GridItem justifySelf='flex-end'>
-            <AddButton
-              className='object-property-expand'
-              onClick={onAddClick(schema)}
-              disabled={disabled || readonly}
-              uiSchema={uiSchema}
-              registry={registry}
-            />
-          </GridItem>
-        )}
-      </Grid> */}
+      </Grid>
+      {canExpand<T, S, F>(schema, uiSchema, formData) && (
+        <GridItem justifySelf="flex-end">
+          <AddButton
+            className="object-property-expand"
+            onClick={onAddClick(schema)}
+            disabled={disabled || readonly}
+            uiSchema={uiSchema}
+            registry={registry}
+          />
+        </GridItem>
+      )}
     </>
-  );
+  )
 }
--- src/views/Connectors/Sources/SourcesForm/SourceConfigForm/SourceConfigForm.tsx
+++ src/views/Connectors/Sources/SourcesForm/SourceConfigForm/SourceConfigForm.tsx
@@ -12,7 +12,7 @@ import SourceFormFooter from "@/views/Connectors/Sources/SourcesForm/SourceFormF
 import Loader from "@/components/Loader";
 import { processFormData } from "@/views/Connectors/helpers";
 import ContentContainer from "@/components/ContentContainer";
-import ObjectFieldTemplate from "@/views/Connectors/Sources/SourcesForm/SourceConfigForm/MWObjectFieldTemplate";
+import MWObjectFieldTemplate from "@/views/Connectors/Sources/SourcesForm/SourceConfigForm/MWObjectFieldTemplate";
 
 const SourceConfigForm = (): JSX.Element | null => {
   const { state, stepInfo, handleMoveForward } = useContext(SteppedFormContext);
@@ -46,15 +46,10 @@ const SourceConfigForm = (): JSX.Element | null => {
       <ContentContainer>
         <Box backgroundColor="gray.200" padding="20px" borderRadius="8px">
           <Form
-            uiSchema={{
-              'ui:layout': {
-                display: 'grid',
-                cols: 2,
-              },
-            }}
+            uiSchema={uiSchema}
             schema={connectorSchema}
             validator={validator}
-            templates={{ObjectFieldTemplate: ObjectFieldTemplate}}
+            templates={{ObjectFieldTemplate: MWObjectFieldTemplate}}
             onSubmit={({ formData }) => handleFormSubmit(formData)}
           >
             <SourceFormFooter ctaName="Continue" ctaType="submit" />
@@ -65,92 +60,96 @@ const SourceConfigForm = (): JSX.Element | null => {
   );
 };
 
-/**
-{
-	"connection_specification": {
-		"$schema": "http://json-schema.org/draft-07/schema#",
-		"title": "Amazon Redshift",
-		"type": "object",
-		"required": [
-			"host",
-			"port",
-			"database",
-			"schema"
-		],
-		"properties": {
-			"credentials": {
-				"title": "",
-				"type": "object",
-				"required": [
-					"auth_type",
-					"username",
-					"password"
-				],
-				"properties": {
-					"auth_type": {
-						"type": "string",
-						"default": "username/password",
-						"order": 0,
-						"readOnly": true
-					},
-					"username": {
-						"description": "Username refers to your individual Redshift login credentials. At a minimum, the user associated with these credentials must be granted read access to the data intended for synchronization.",
-						"examples": [
-							"REDSHIFT_USER"
-						],
-						"type": "string",
-						"title": "Username",
-						"order": 1
-					},
-					"password": {
-						"description": "This field requires the password associated with the user account specified in the preceding section.",
-						"type": "string",
-						"multiwoven_secret": true,
-						"title": "Password",
-						"order": 2
-					}
-				},
-				"order": 0
-			},
-			"host": {
-				"description": "The hostname or IP address of your Redshift cluster represents a critical connectivity parameter. To retrieve this information, access the Redshift web console, proceed to the Clusters panel, and select your specific cluster. Within the cluster's details, locate and copy the Endpoint string, ensuring to omit the port number and database name appended at the conclusion.",
-				"examples": [
-					"example-redshift-cluster.abcdefg.us-west-2.redshift.amazonaws.com"
-				],
-				"type": "string",
-				"title": "Host",
-				"order": 1
-			},
-			"port": {
-				"description": "The port number for your Redshift cluster, which defaults to 5439, may vary based on your configuration. To verify the specific port number assigned to your cluster, access the Redshift web console, proceed to the Clusters panel, and select your cluster. You can find the port number displayed within the Properties tab.",
-				"examples": [
-					"5439"
-				],
-				"type": "string",
-				"title": "Port",
-				"order": 2
-			},
-			"database": {
-				"description": "The specific Redshift database to connect to.",
-				"examples": [
-					"REDSHIFT_DB"
-				],
-				"type": "string",
-				"title": "Database",
-				"order": 3
-			},
-			"schema": {
-				"description": "The schema within the Redshift database.",
-				"examples": [
-					"REDSHIFT_SCHEMA"
-				],
-				"type": "string",
-				"title": "Schema",
-				"order": 4
-			}
-		}
-	}
+const uiSchema = {
+  "ui:order": ["host", "port", "database", "credentials", "schema"],
+  "ui:layout": {
+    display: "grid",
+    cols: 2,
+    colspans: [2, 1, 1, 2, 2]
+  },
+  credentials: {
+    "ui:layout": {
+      display: "grid",
+      cols: 2,
+      colspans: [1, 1]
+    },
+    auth_type: {
+      "ui:widget": "hidden"
+    }
+  }
 }
- */
+
+// @ts-expect-error This is for illustration
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const schema = {
+  $schema: "http://json-schema.org/draft-07/schema#",
+  title: "Amazon Redshift",
+  type: "object",
+  required: ["host", "port", "database", "schema"],
+  properties: {
+    credentials: {
+      title: "",
+      type: "object",
+      required: ["auth_type", "username", "password"],
+      properties: {
+        auth_type: {
+          type: "string",
+          default: "username/password",
+          order: 0,
+          readOnly: true,
+        },
+        username: {
+          description:
+            "Username refers to your individual Redshift login credentials. At a minimum, the user associated with these credentials must be granted read access to the data intended for synchronization.",
+          examples: ["REDSHIFT_USER"],
+          type: "string",
+          title: "Username",
+          order: 1,
+        },
+        password: {
+          description:
+            "This field requires the password associated with the user account specified in the preceding section.",
+          type: "string",
+          multiwoven_secret: true,
+          title: "Password",
+          order: 2,
+        },
+      },
+      order: 0,
+    },
+    host: {
+      description:
+        "The hostname or IP address of your Redshift cluster represents a critical connectivity parameter. To retrieve this information, access the Redshift web console, proceed to the Clusters panel, and select your specific cluster. Within the cluster's details, locate and copy the Endpoint string, ensuring to omit the port number and database name appended at the conclusion.",
+      examples: [
+        "example-redshift-cluster.abcdefg.us-west-2.redshift.amazonaws.com",
+      ],
+      type: "string",
+      title: "Host",
+      order: 1,
+    },
+    port: {
+      description:
+        "The port number for your Redshift cluster, which defaults to 5439, may vary based on your configuration. To verify the specific port number assigned to your cluster, access the Redshift web console, proceed to the Clusters panel, and select your cluster. You can find the port number displayed within the Properties tab.",
+      examples: ["5439"],
+      type: "string",
+      title: "Port",
+      order: 2,
+    },
+    database: {
+      description: "The specific Redshift database to connect to.",
+      examples: ["REDSHIFT_DB"],
+      type: "string",
+      title: "Database",
+      order: 3,
+    },
+    schema: {
+      description: "The schema within the Redshift database.",
+      examples: ["REDSHIFT_SCHEMA"],
+      type: "string",
+      title: "Schema",
+      order: 4,
+    },
+  },
+}
 
 export default SourceConfigForm;
