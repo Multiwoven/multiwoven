7274bb044739c628944e2403c8de637a1e0508c1
sumitd94 <sumitdhanania94@gmail.com>
2024-02-19 15:50:06 +0530

    style: update bg color for the list screens

--- src/views/MainLayout/MainLayout.tsx
+++ src/views/MainLayout/MainLayout.tsx
@@ -1,24 +1,24 @@
-import { useUiConfig } from "@/utils/hooks";
-import Sidebar from "@/views/Sidebar";
-import { Box } from "@chakra-ui/layout";
-import { Outlet } from "react-router-dom";
+import { useUiConfig } from '@/utils/hooks';
+import Sidebar from '@/views/Sidebar';
+import { Box } from '@chakra-ui/layout';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = (): JSX.Element => {
   const { contentContainerId } = useUiConfig();
   return (
-    <Box display="flex" width={"100%"} overflow="hidden" maxHeight="100vh">
+    <Box display='flex' width={'100%'} overflow='hidden' maxHeight='100vh'>
       <Sidebar />
       <Box
         pl={0}
-        bg={"#fcfcfc"}
-        width={"100%"}
-        maxW={"100%"}
-        display="flex"
+        width={'100%'}
+        maxW={'100%'}
+        display='flex'
         flex={1}
-        flexDir="column"
-        className="flex"
-        overflow="scroll"
+        flexDir='column'
+        className='flex'
+        overflow='scroll'
         id={contentContainerId}
+        backgroundColor='gray.200'
       >
         <Outlet />
       </Box>
