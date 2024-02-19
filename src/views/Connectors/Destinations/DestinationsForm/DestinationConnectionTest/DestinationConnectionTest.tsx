f5978f8a1aa698c790725c34ac5aa093d15969f2
sumitd94 <sumitdhanania94@gmail.com>
2024-02-20 02:11:19 +0530

    style: update test destination screen

--- src/views/Connectors/Destinations/DestinationsForm/DestinationConnectionTest/DestinationConnectionTest.tsx
+++ src/views/Connectors/Destinations/DestinationsForm/DestinationConnectionTest/DestinationConnectionTest.tsx
@@ -1,7 +1,7 @@
-import { SteppedFormContext } from "@/components/SteppedForm/SteppedForm";
-import { getConnectionStatus } from "@/services/connectors";
-import { processConnectorConfigData } from "@/views/Connectors/helpers";
-import { TestConnectionPayload } from "@/views/Connectors/types";
+import { SteppedFormContext } from '@/components/SteppedForm/SteppedForm';
+import { getConnectionStatus } from '@/services/connectors';
+import { processConnectorConfigData } from '@/views/Connectors/helpers';
+import { TestConnectionPayload } from '@/views/Connectors/types';
 import {
   Alert,
   AlertDescription,
@@ -11,20 +11,21 @@ import {
   Icon,
   Spinner,
   Text,
-} from "@chakra-ui/react";
-import { useQuery } from "@tanstack/react-query";
-import { useContext, useMemo } from "react";
-import { CONNECTION_STATUS } from "@/views/Connectors/constant";
-import { FiAlertOctagon, FiCheck } from "react-icons/fi";
-import { useUiConfig } from "@/utils/hooks";
-import SourceFormFooter from "@/views/Connectors/Sources/SourcesForm/SourceFormFooter";
+} from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import { useContext, useMemo } from 'react';
+import { CONNECTION_STATUS } from '@/views/Connectors/constant';
+import { FiAlertOctagon, FiCheck } from 'react-icons/fi';
+import { useUiConfig } from '@/utils/hooks';
+import SourceFormFooter from '@/views/Connectors/Sources/SourcesForm/SourceFormFooter';
+import ContentContainer from '@/components/ContentContainer';
 
-const CONNECT_TO_DESTINATION_KEY = "destinationConfig";
+const CONNECT_TO_DESTINATION_KEY = 'destinationConfig';
 
 const STATUS_COLOR_MAP = {
-  success: "green.400",
-  failed: "red.400",
-  loading: "gray.800",
+  success: 'green.400',
+  failed: 'red.400',
+  loading: 'gray.800',
 };
 
 const DestinationConnectionTest = (): JSX.Element | null => {
@@ -33,7 +34,7 @@ const DestinationConnectionTest = (): JSX.Element | null => {
   const { maxContentWidth } = useUiConfig();
 
   const selectedDestination = forms.find(
-    ({ stepKey }) => stepKey === "destination"
+    ({ stepKey }) => stepKey === 'destination'
   )?.data?.destination as string;
 
   const destinationConfigForm = forms.find(
@@ -47,7 +48,7 @@ const DestinationConnectionTest = (): JSX.Element | null => {
       processConnectorConfigData(
         destinationConfig,
         selectedDestination,
-        "destination"
+        'destination'
       ),
     [forms]
   );
@@ -57,7 +58,7 @@ const DestinationConnectionTest = (): JSX.Element | null => {
     refetch: retryDestinationConnection,
     isFetching,
   } = useQuery({
-    queryKey: ["connector_definition", "test-connection", "destination"],
+    queryKey: ['connector_definition', 'test-connection', 'destination'],
     queryFn: () =>
       getConnectionStatus(processedDestinationConfig as TestConnectionPayload),
     enabled: !!processedDestinationConfig,
@@ -66,100 +67,146 @@ const DestinationConnectionTest = (): JSX.Element | null => {
   });
 
   const isAnyFailed =
-    connectionResponse?.connection_status.status !== "succeeded";
+    connectionResponse?.connection_status.status !== 'succeeded';
 
   const handleOnContinueClick = () => {
     handleMoveForward(stepInfo?.formKey as string, processedDestinationConfig);
   };
 
   return (
-    <Box display="flex" justifyContent="center">
-      <Box maxWidth={maxContentWidth} width="100%">
-        <Box
-          padding="24px"
-          backgroundColor="gray.100"
-          borderRadius="8px"
-          marginBottom="20px"
-        >
-          {CONNECTION_STATUS.map(({ status }) => {
-            const statusMetaInfo = status({
-              data: connectionResponse,
-              isLoading: isFetching,
-              configFormData: destinationConfig,
-              datasource: selectedDestination,
-            });
+    <Box width='100%' display='flex' justifyContent='center'>
+      <ContentContainer>
+        <Box maxWidth={maxContentWidth} width='100%'>
+          <Box
+            padding='24px'
+            backgroundColor='gray.300'
+            borderRadius='8px'
+            marginBottom='16px'
+          >
+            {CONNECTION_STATUS.map(({ status }) => {
+              const statusMetaInfo = status({
+                data: connectionResponse,
+                isLoading: isFetching,
+                configFormData: destinationConfig,
+                datasource: selectedDestination,
+              });
 
-            return (
-              <Box
-                key={statusMetaInfo.text}
-                display="flex"
-                marginBottom="20px"
-                alignItems="center"
-                height="30px"
+              return (
+                <Box
+                  key={statusMetaInfo.text}
+                  display='flex'
+                  marginBottom='24px'
+                  alignItems='center'
+                  height='30px'
+                >
+                  <Box>
+                    {statusMetaInfo.status === 'loading' ? (
+                      <Spinner height='20px' width='20px' />
+                    ) : null}
+                    {statusMetaInfo.status === 'success' ? (
+                      <Box
+                        display='flex'
+                        alignItems='center'
+                        justifyContent='center'
+                        backgroundColor={STATUS_COLOR_MAP.success}
+                        padding='5px'
+                        borderRadius='50%'
+                        height='20px'
+                        width='20px'
+                      >
+                        <Icon as={FiCheck} boxSize={4} color='gray.100' />
+                      </Box>
+                    ) : null}
+
+                    {statusMetaInfo.status === 'failed' ? (
+                      <Box
+                        display='flex'
+                        alignItems='center'
+                        justifyContent='center'
+                        backgroundColor={STATUS_COLOR_MAP.failed}
+                        padding='5px'
+                        borderRadius='50%'
+                        height='20px'
+                        width='20px'
+                      >
+                        <Icon
+                          as={FiAlertOctagon}
+                          boxSize={4}
+                          color='gray.100'
+                        />
+                      </Box>
+                    ) : null}
+                  </Box>
+                  <Box marginLeft='10px'>
+                    <Text
+                      fontWeight='semibold'
+                      size='md'
+                      color={
+                        statusMetaInfo.status === 'loading'
+                          ? 'black.100'
+                          : 'black.500'
+                      }
+                    >
+                      {statusMetaInfo.text}
+                    </Text>
+                  </Box>
+                </Box>
+              );
+            })}
+            {isAnyFailed && connectionResponse ? (
+              <Button
+                borderColor='gray.500'
+                isDisabled={isFetching}
+                onClick={() => retryDestinationConnection()}
+                minWidth={0}
+                width='auto'
+                borderStyle='solid'
+                borderWidth='1px'
+                backgroundColor='gray.200'
+                color='black.500'
+                fontSize='12px'
+                _hover={{ bgColor: 'gray.300', color: 'black' }}
+                height='32px'
               >
-                <Box>
-                  {statusMetaInfo.status === "loading" ? <Spinner /> : null}
-                  {statusMetaInfo.status === "success" ? (
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="center"
-                      backgroundColor={STATUS_COLOR_MAP.success}
-                      padding="5px"
-                      borderRadius="50%"
-                    >
-                      <Icon as={FiCheck} boxSize={4} color="gray.100" />
-                    </Box>
-                  ) : null}
-
-                  {statusMetaInfo.status === "failed" ? (
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="center"
-                      backgroundColor={STATUS_COLOR_MAP.failed}
-                      padding="5px"
-                      borderRadius="50%"
-                    >
-                      <Icon as={FiAlertOctagon} boxSize={4} color="gray.100" />
-                    </Box>
-                  ) : null}
-                </Box>
-                <Box marginLeft="10px">
-                  <Text fontWeight="600">{statusMetaInfo.text}</Text>
-                </Box>
-              </Box>
-            );
-          })}
-          {isAnyFailed && connectionResponse ? (
-            <Button
-              variant="outline"
-              borderColor="gray.500"
-              isDisabled={isFetching}
-              onClick={() => retryDestinationConnection()}
+                Test Again
+              </Button>
+            ) : null}
+          </Box>
+          {!isFetching ? (
+            <Alert
+              status={isAnyFailed ? 'error' : 'success'}
+              borderRadius='8px'
+              backgroundColor={isAnyFailed ? 'error.100' : 'success.100'}
+              paddingX='16px'
+              paddingY='12px'
             >
-              Test Again
-            </Button>
+              <Box>
+                <AlertTitle
+                  fontSize='14px'
+                  fontWeight='semibold'
+                  letterSpacing='-0.14px'
+                >
+                  {isAnyFailed
+                    ? 'Could not open a connection to remote host'
+                    : 'Connected successfully!'}
+                </AlertTitle>
+                <AlertDescription>
+                  {isAnyFailed
+                    ? connectionResponse?.connection_status.message
+                    : `All tests passed. Continue to finish setting up your ${selectedDestination} Source`}
+                </AlertDescription>
+              </Box>
+            </Alert>
           ) : null}
         </Box>
-        {!isFetching ? (
-          <Alert status={isAnyFailed ? "error" : "success"} borderRadius="8px">
-            <Box>
-              <AlertTitle>
-                {isAnyFailed
-                  ? "Could not open a connection to remote host"
-                  : "Connected successfully!"}
-              </AlertTitle>
-              <AlertDescription>
-                {isAnyFailed
-                  ? connectionResponse?.connection_status.message
-                  : `All tests passed. Continue to finish setting up your ${selectedDestination} Source`}
-              </AlertDescription>
-            </Box>
-          </Alert>
-        ) : null}
-      </Box>
-      <SourceFormFooter ctaName="Continue" onCtaClick={handleOnContinueClick} />
+        <SourceFormFooter
+          ctaName='Continue'
+          onCtaClick={handleOnContinueClick}
+          isContinueCtaRequired
+          isBackRequired
+          isDocumentsSectionRequired
+        />
+      </ContentContainer>
     </Box>
   );
 };
