1069bc81664a770ea03a093c464564d40687a0a5
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-02-21 11:42:32 +0530

    Added API_HOST ENV check

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export const domain = "https://api.multiwoven.com/api/v1";
+export const domain = import.meta.env.VITE_API_HOST || "http://localhost:3000";
+console.log(domain);
+
 export const axiosInstance = axios.create({
   baseURL: domain,
 });
