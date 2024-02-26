85e7bdb38253c5a5696a73bdb75323a8590a50bf
Afthab <afthab01@gmail.com>
2024-02-26 11:23:50 +0530

    feat: fix pr comments

--- app/interactors/reports/activity_report.rb
+++ app/interactors/reports/activity_report.rb
@@ -22,46 +22,49 @@ module Reports
     }.freeze
 
     def call
-      type = context.type
+      raise ArgumentError, "Invalid type  #{context.type}" unless respond_to?(context.type, true)
 
-      case type.to_sym
-      when :workspace_activity
-        generate_workspace_activity_report
-      else
-        raise ArgumentError, "Invalid type: #{type}"
-      end
+      send(context.type)
     end
 
     private
 
-    def generate_workspace_activity_report
-      connector_id = context.connector_id
-      time_period = context.time_period || :one_week
-      metric = context.metric || :all
-
-      start_time = calculate_start_time(time_period)
-      end_time = Time.zone.now
-      @interval = (((end_time - start_time) / 60) / SLICE_SIZE).to_i
-      @sync_activity = fetch_sync_activity(start_time, end_time)
-      filter_sync_activity(connector_id) if connector_id.present?
-
-      context.workspace_activity = send(metric)
+    def filter_params
+      {
+        connector_id: context.connector_id,
+        time_period: context.time_period || :one_week,
+        metric: context.metric || :all
+      }
     end
 
-    def fetch_sync_activity(start_time, end_time)
-      context.workspace.sync_runs.where(created_at: start_time..end_time)
+    def workspace_activity
+      params = filter_params
+      start_time = calculate_start_time(params[:time_period])
+      end_time = Time.zone.now
+      @interval = ((end_time - start_time) / 60 / SLICE_SIZE).to_i
+      @workspace_activities = fetch_activities(start_time, end_time)
+      filter_sync_activity(params[:connector_id]) if params[:connector_id].present?
+      context.workspace_activity = send(params[:metric])
+    end
+
+    def fetch_activities(start_time, end_time)
+      scope.sync_runs.where(created_at: start_time..end_time)
     end
 
     def filter_sync_activity(connector_id)
-      @sync_activity = @sync_activity.where("source_id = :connector_id OR destination_id = :connector_id",
-                                            connector_id:)
+      @workspace_activities = @workspace_activities.where("source_id = :connector_id OR destination_id = :connector_id",
+                                                          connector_id:)
+    end
+
+    def scope
+      context.workspace if context[:type].match?("workspace_activity")
     end
 
     def sync_run_triggered
-      return [] unless @sync_activity
+      return [] unless @workspace_activities
 
-      total_grouped = @sync_activity.group_by_minute(:created_at, n: @interval).count
-      error_grouped = @sync_activity.where.not(error: nil).group_by_minute(:created_at, n: @interval).count
+      total_grouped = @workspace_activities.group_by_minute(:created_at, n: @interval).count
+      error_grouped = @workspace_activities.where.not(error: nil).group_by_minute(:created_at, n: @interval).count
 
       total_grouped.map do |time_interval, total_count|
         {
@@ -74,9 +77,9 @@ module Reports
     end
 
     def total_sync_run_rows
-      return [] unless @sync_activity
+      return [] unless @workspace_activities
 
-      grouped_data = @sync_activity.group_by_minute(:created_at, n: @interval)
+      grouped_data = @workspace_activities.group_by_minute(:created_at, n: @interval)
       successful_group_data = grouped_data.sum(:successful_rows)
       failed_group_data = grouped_data.sum(:failed_rows)
 
