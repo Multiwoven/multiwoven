dcd0392cc168f014b455994fbfab792560e9b859
afthab vp <afthab01@gmail.com>
2024-03-04 19:47:38 +0530

    bug: create sync run when pending sync run is absent (#91)
    * bug: fix the create sync run issue for any sync run that exists for sync id
    
    * bug: fix description

--- app/temporal/activities/create_sync_run_activity.rb
+++ app/temporal/activities/create_sync_run_activity.rb
@@ -4,7 +4,7 @@ module Activities
   class CreateSyncRunActivity < Temporal::Activity
     def execute(sync_id)
       sync = Sync.find(sync_id)
-      sync_run = SyncRun.find_or_initialize_by(sync_id:) do |run|
+      sync_run = SyncRun.find_or_initialize_by(sync_id:, status: :pending) do |run|
         run.workspace_id = sync.workspace_id
         run.source_id = sync.source_id
         run.destination_id = sync.destination_id
--- spec/temporal/activities/create_sync_run_activity_spec.rb
+++ spec/temporal/activities/create_sync_run_activity_spec.rb
@@ -12,15 +12,35 @@ RSpec.describe Activities::CreateSyncRunActivity do
     let!(:sync) { create(:sync, sync_interval: 3, sync_interval_unit: "hours", source:, destination:) }
     let(:mock_context) { double("context") }
     let(:activity) { Activities::CreateSyncRunActivity.new(mock_context) }
-    it "creates a new SyncRun for the provided Sync ID and check status pending" do
-      sync_run_id = activity.execute(sync.id)
-      sync_run = SyncRun.find(sync_run_id)
-      expect(sync_run).to have_state(:pending)
-      expect(sync_run.sync_id).to eq(sync.id)
-      expect(sync_run.workspace_id).to eq(sync.workspace_id)
-      expect(sync_run.source_id).to eq(sync.source_id)
-      expect(sync_run.destination_id).to eq(sync.destination_id)
-      expect(sync_run.model_id).to eq(sync.model_id)
+
+    context "when no pending SyncRun exists" do
+      it "creates a new SyncRun with pending status" do
+        expect do
+          sync_run_id = activity.execute(sync.id)
+          sync_run = SyncRun.find(sync_run_id)
+          expect(sync_run).to have_state(:pending)
+          expect(sync_run.sync_id).to eq(sync.id)
+          expect(sync_run.workspace_id).to eq(sync.workspace_id)
+          expect(sync_run.source_id).to eq(sync.source_id)
+          expect(sync_run.destination_id).to eq(sync.destination_id)
+          expect(sync_run.model_id).to eq(sync.model_id)
+        end.to change(SyncRun, :count).by(1)
+      end
+    end
+
+    context "when a pending SyncRun started exists need to create new with pending" do
+      before do
+        create(:sync_run, sync:, status: :success)
+      end
+
+      it "create a new SyncRun" do
+        expect do
+          sync_run_id = activity.execute(sync.id)
+          sync_run = SyncRun.find(sync_run_id)
+          expect(sync_run).to have_state(:pending)
+          expect(sync_run.sync_id).to eq(sync.id)
+        end.to change(SyncRun, :count).by(1)
+      end
     end
   end
 end
