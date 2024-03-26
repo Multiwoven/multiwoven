b20bd6bb23209a821698bbf37dcce710b5b116bd
datafloyd <subinthattaparambil@gmail.com>
2024-03-26 13:06:33 +0530

    Enable postgres destination (#108)

--- lib/multiwoven/integrations/rollout.rb
+++ lib/multiwoven/integrations/rollout.rb
@@ -2,7 +2,7 @@
 
 module Multiwoven
   module Integrations
-    VERSION = "0.1.46"
+    VERSION = "0.1.47"
 
     ENABLED_SOURCES = %w[
       Snowflake
@@ -24,6 +24,7 @@ module Multiwoven
       Stripe
       SalesforceConsumerGoodsCloud
       Sftp
+      Postgresql
     ].freeze
   end
 end
