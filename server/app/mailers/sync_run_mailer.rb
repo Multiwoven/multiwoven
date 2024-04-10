1c063e2414fbb97c79160292042c0ec8b8b45bec
Karthik Sivadas <karthiksivadas@outlook.com>
2024-04-10 08:44:43 +0530

    Update email config and add email notification push on sync run status change

--- server/app/mailers/sync_run_mailer.rb
+++ server/app/mailers/sync_run_mailer.rb
@@ -1,9 +1,18 @@
+# frozen_string_literal: true
+
 class SyncRunMailer < ApplicationMailer
-  default from: ENV['SMTP_USERNAME']
+  default from: Rails.configuration.x.mail_from
 
   def status_email
     @sync_run = params[:sync_run]
+    @sync = @sync_run.sync
     @sync_status = @sync_run.status
-    mail(to: params[:recipient], subject: "Sync run status update")
+    @source_connector_name = @sync_run.sync.source.name
+    @destination_connector_name = @sync_run.sync.destination.name
+    host = Rails.configuration.action_mailer.default_url_options[:host]
+    @sync_run_url = "#{host}/activate/syncs/#{@sync.id}/run/#{@sync_run.id}"
+    mail(to: params[:recipient], subject: "Sync run status update") do |format|
+      format.html { render layout: false }
+    end
   end
 end
