bc76c4622e925a24185a88ad01762ea860b8b13e
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-04-12 09:38:47 +0530

    refactor: changed finalize form width and color

--- ui/src/views/Models/ModelsForm/FinalizeModel/FinalizeModel.tsx
+++ ui/src/views/Models/ModelsForm/FinalizeModel/FinalizeModel.tsx
@@ -24,6 +24,7 @@ import { createNewModel } from '@/services/models';
 import SourceFormFooter from '@/views/Connectors/Sources/SourcesForm/SourceFormFooter';
 import { CustomToastStatus } from '@/components/Toast/index';
 import useCustomToast from '@/hooks/useCustomToast';
+import ContentContainer from '@/components/ContentContainer';
 
 type ModelConfig = {
   id: number;
@@ -97,104 +98,106 @@ const FinalizeModel = (): JSX.Element => {
   }
 
   return (
-    <>
-      <Box bgColor='gray.300' px={6} py={4} marginTop={6} marginX='30px' borderRadius='8px'>
-        <Text mb={6} fontWeight='semibold' size='md'>
-          Finalize settings for this Model
-        </Text>
-        <Formik
-          initialValues={{
-            modelName: '',
-            description: '',
-            primaryKey: '',
-          }}
-          validationSchema={validationSchema}
-          onSubmit={(values) => {
-            handleModelSubmit(values);
-          }}
-        >
-          <Form>
-            <VStack spacing={5}>
-              <FormControl>
-                <FormLabel htmlFor='modelName' fontSize='sm' fontWeight='semibold'>
-                  Model Name
-                </FormLabel>
-                <Field
-                  as={Input}
-                  id='modelName'
-                  name='modelName'
-                  placeholder='Enter a name'
-                  bgColor='white'
-                  borderWidth='1'
-                  borderStyle='solid'
-                  borderColor='gray.400'
-                  fontSize='sm'
-                />
-                <Text color='red.500' fontSize='sm'>
-                  <ErrorMessage name='modelName' />
-                </Text>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor='description' fontWeight='semibold'>
-                  <Flex alignItems='center' fontSize='sm'>
-                    Description{' '}
-                    <Text ml={2} size='xs' color='gray.600' fontWeight={400}>
-                      {' '}
-                      (optional)
-                    </Text>
-                  </Flex>
-                </FormLabel>
-                <Field
-                  as={Textarea}
-                  id='description'
-                  name='description'
-                  placeholder='Enter a description'
-                  bgColor='white'
-                  borderWidth='1'
-                  borderStyle='solid'
-                  borderColor='gray.400'
-                  fontSize='sm'
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor='primaryKey' fontSize='sm' fontWeight='semibold'>
-                  Primary Key
-                </FormLabel>
-                <Field
-                  as={Select}
-                  placeholder='Select Primary Key'
-                  name='primaryKey'
-                  bgColor='white'
-                  w='lg'
-                  borderWidth='1'
-                  borderStyle='solid'
-                  borderColor='gray.400'
-                  fontSize='sm'
-                >
-                  {(defineModelData.data.defineModel.columns ?? []).map(
-                    ({ key, name }: ColumnMapType, index: number) => (
-                      <option key={index} value={key}>
-                        {name}
-                      </option>
-                    ),
-                  )}
-                </Field>
-                <Text color='red.500' fontSize='sm'>
-                  <ErrorMessage name='primaryKey' />
-                </Text>
-              </FormControl>
-            </VStack>
-            <SourceFormFooter
-              isCtaLoading={isLoading}
-              ctaType='submit'
-              ctaName='Finish'
-              isBackRequired
-              isContinueCtaRequired
-            />
-          </Form>
-        </Formik>
-      </Box>
-    </>
+    <Box display='flex' width='100%' justifyContent='center'>
+      <ContentContainer>
+        <Box bgColor='gray.200' px={6} py={4} marginTop={6} marginX='30px' borderRadius='8px'>
+          <Text mb={6} fontWeight='semibold' size='md'>
+            Finalize settings for this Model
+          </Text>
+          <Formik
+            initialValues={{
+              modelName: '',
+              description: '',
+              primaryKey: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+              handleModelSubmit(values);
+            }}
+          >
+            <Form>
+              <VStack spacing={5}>
+                <FormControl>
+                  <FormLabel htmlFor='modelName' fontSize='sm' fontWeight='semibold'>
+                    Model Name
+                  </FormLabel>
+                  <Field
+                    as={Input}
+                    id='modelName'
+                    name='modelName'
+                    placeholder='Enter a name'
+                    bgColor='white'
+                    borderWidth='1'
+                    borderStyle='solid'
+                    borderColor='gray.400'
+                    fontSize='sm'
+                  />
+                  <Text color='red.500' fontSize='sm'>
+                    <ErrorMessage name='modelName' />
+                  </Text>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor='description' fontWeight='semibold'>
+                    <Flex alignItems='center' fontSize='sm'>
+                      Description{' '}
+                      <Text ml={2} size='xs' color='gray.600' fontWeight={400}>
+                        {' '}
+                        (optional)
+                      </Text>
+                    </Flex>
+                  </FormLabel>
+                  <Field
+                    as={Textarea}
+                    id='description'
+                    name='description'
+                    placeholder='Enter a description'
+                    bgColor='white'
+                    borderWidth='1'
+                    borderStyle='solid'
+                    borderColor='gray.400'
+                    fontSize='sm'
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor='primaryKey' fontSize='sm' fontWeight='semibold'>
+                    Primary Key
+                  </FormLabel>
+                  <Field
+                    as={Select}
+                    placeholder='Select Primary Key'
+                    name='primaryKey'
+                    bgColor='white'
+                    w='lg'
+                    borderWidth='1'
+                    borderStyle='solid'
+                    borderColor='gray.400'
+                    fontSize='sm'
+                  >
+                    {(defineModelData.data.defineModel.columns ?? []).map(
+                      ({ key, name }: ColumnMapType, index: number) => (
+                        <option key={index} value={key}>
+                          {name}
+                        </option>
+                      ),
+                    )}
+                  </Field>
+                  <Text color='red.500' fontSize='sm'>
+                    <ErrorMessage name='primaryKey' />
+                  </Text>
+                </FormControl>
+              </VStack>
+              <SourceFormFooter
+                isCtaLoading={isLoading}
+                ctaType='submit'
+                ctaName='Finish'
+                isBackRequired
+                isContinueCtaRequired
+              />
+            </Form>
+          </Formik>
+        </Box>
+      </ContentContainer>
+    </Box>
   );
 };
 
