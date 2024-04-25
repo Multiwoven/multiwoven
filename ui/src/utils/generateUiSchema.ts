95c7c71247298da5d7b11ff45167063e84fb50ee
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-04-25 15:03:54 +0530

    feat: added dynamic UI Schema generation

---
+++ ui/src/utils/generateUiSchema.ts
@@ -0,0 +1,26 @@
+export function generateUiSchema(
+  schemaProperties: Object,
+  uiSchema: Record<string, any> = {},
+  path: string[] = [],
+): Record<string, string> {
+  Object.entries(schemaProperties).forEach(([key, value]) => {
+    if (key === 'properties' && typeof value === 'object' && value !== null) {
+      generateUiSchema(value, uiSchema, path);
+    } else if (typeof value === 'object' && value !== null) {
+      const nestedObject = key === 'properties' ? path : path.concat(key);
+      generateUiSchema(value, uiSchema, nestedObject);
+    } else if (key === 'multiwoven_secret' && value === true) {
+      let current = uiSchema;
+      path.forEach((p, i) => {
+        if (i === path.length - 1) {
+          current[p] = { 'ui:widget': 'password' };
+        } else {
+          current[p] = current[p] || {};
+        }
+        current = current[p];
+      });
+    }
+  });
+
+  return uiSchema;
+}
