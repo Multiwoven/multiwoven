a6d37c5286814e79115d210a5d3f1c6859453393
Praneeth AIS <praneeth.chandu@squared.ai>
2024-04-30 17:14:21 -0400

    separating meta data test from write test

--- integrations/spec/multiwoven/integrations/destination/zendesk/client_spec.rb
+++ integrations/spec/multiwoven/integrations/destination/zendesk/client_spec.rb
@@ -161,14 +161,7 @@ RSpec.describe Multiwoven::Integrations::Destination::Zendesk::Client do
             end
         end
 
-        describe "#meta_data" do
-            # change this to rollout validation for all connector rolling out
-            it "client class_name and meta name is same" do
-                meta_name = client.class.to_s.split("::")[-2]
-                expect(client.send(:meta_data)[:data][:name]).to eq(meta_name)
-            end
-        end
-
+        
         # context "when the write operation fails" do
         #     it "increments the failure count" do
         #         allow(client).to receive_message_chain(:tickets, :create!).and_raise(StandardError.new('Failed to create ticket'))
@@ -180,5 +173,13 @@ RSpec.describe Multiwoven::Integrations::Destination::Zendesk::Client do
         #         # expect(client.write(sync_config, records)).to include(failures: records.size)
         #     end
         # end
-  end
+    end
+    
+    describe "#meta_data" do
+        # change this to rollout validation for all connector rolling out
+        it "client class_name and meta name is same" do
+            meta_name = client.class.to_s.split("::")[-2]
+            expect(client.send(:meta_data)[:data][:name]).to eq(meta_name)
+        end
+    end
 end
