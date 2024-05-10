f13e0db96f86c0cc58a8d444d61891272cc26cf7
Tushar Selvakumar <54372016+macintushar@users.noreply.github.com>
2024-05-11 00:12:54 +0530

    fix: added hardcoded staging and prod urls (#30)

--- ui/src/services/axios.ts
+++ ui/src/services/axios.ts
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const DOMAIN =
-  window?.VITE_API_HOST !== '__VITE_API_HOST__' && window?.VITE_API_HOST !== 'undefined'
-    ? `${window?.VITE_API_HOST}/api/v1/`
-    : import.meta.env.VITE_API_HOST
-      ? `${import.meta.env.VITE_API_HOST}/api/v1/`
-      : 'http://localhost:3000/api/v1/';
+// const DOMAIN =
+//   window?.VITE_API_HOST !== '__VITE_API_HOST__' && window?.VITE_API_HOST !== 'undefined'
+//     ? `${window?.VITE_API_HOST}/api/v1/`
+//     : import.meta.env.VITE_API_HOST
+//       ? `${import.meta.env.VITE_API_HOST}/api/v1/`
+//       : 'http://localhost:3000/api/v1/';
 
-export const domain = DOMAIN;
+// export const domain = DOMAIN;
+const windowURL = window.location.href;
+const isStaging = windowURL.includes('staging');
+
+export const domain = isStaging
+  ? 'https://api-staging.squared.ai/api/v1/'
+  : 'https://api.squared.ai/api/v1/';
 export const axiosInstance = axios.create({
   baseURL: domain,
 });
