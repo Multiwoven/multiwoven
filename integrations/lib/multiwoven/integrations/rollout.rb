c2407a4afecab025a92152b8db695f390830c7fa
AnthonyR-AI2 <108499681+AnthonyR-AI2@users.noreply.github.com>
2024-05-16 03:16:48 -0400

    feat: Add http destination connector (#31)

--- integrations/lib/multiwoven/integrations/rollout.rb
+++ integrations/lib/multiwoven/integrations/rollout.rb
@@ -2,7 +2,7 @@
 
 module Multiwoven
   module Integrations
-    VERSION = "0.1.64"
+    VERSION = "0.1.65"
 
     ENABLED_SOURCES = %w[
       Snowflake
@@ -25,6 +25,7 @@ module Multiwoven
       SalesforceConsumerGoodsCloud
       Sftp
       Postgresql
+      Http
     ].freeze
   end
 end
