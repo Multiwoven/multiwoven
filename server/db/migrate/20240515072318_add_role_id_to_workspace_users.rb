3e8d58c87bd39332773794f676d72a8431b31314
afthab vp <afthab@multiwoven.com>
2024-05-21 15:42:27 +0530

    [CE] fix: role id null in migration (#143)

--- server/db/migrate/20240515072318_add_role_id_to_workspace_users.rb
+++ server/db/migrate/20240515072318_add_role_id_to_workspace_users.rb
@@ -4,7 +4,7 @@ class AddRoleIdToWorkspaceUsers < ActiveRecord::Migration[7.1]
       remove_column :workspace_users, :role
     end
     unless column_exists?(:workspace_users, :role_id)
-      add_reference :workspace_users, :role, null: false, foreign_key: true, default: nil
+      add_reference :workspace_users, :role, foreign_key: true, default: nil
     end
   end
 end
