45c110bf311af558a75ca3bc6bc12cb652324bc1
datafloyd <subinthattaparambil@gmail.com>
2024-05-24 15:54:53 +0530

    Revert "[CE] feat: add appsignal logging (#147)"
    This reverts commit d06d052f608c14a496afbb7e924dc3fa84aa0a2a.

--- server/config/initializers/logging.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-if ENV["APPSIGNAL_PUSH_API_KEY"]
-  appsignal_logger = Appsignal::Logger.new("rails")
-  tagged_appsignal_logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
-  Rails.logger.broadcast_to(tagged_appsignal_logger)
-end
