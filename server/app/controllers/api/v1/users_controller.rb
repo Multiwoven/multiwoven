ba680e1a64da0402234cd074615824b71a6eff8d
afthab vp <afthab@multiwoven.com>
2024-06-06 16:13:06 +0530

    fix(CE): add role in user info api  response

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class UsersController < ApplicationController
       def me
-        render json: current_user, status: :ok
+        render json: current_user, serializer: UserSerializer, workspace_id: current_workspace.id
       end
     end
   end
