f01942cd935571f4fe1c53faf7ba2e198479410c
afthab vp <afthab@multiwoven.com>
2024-06-13 17:05:17 +0530

    [CE] fix: missing invited user email in template

--- server/app/mailers/devise_mailer.rb
+++ server/app/mailers/devise_mailer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DeviseMailer < Devise::Mailer
+  default from: Rails.configuration.x.mail_from
+
   def invitation_instructions(record, token, opts = {})
     @workspace = opts[:workspace]
     @role = opts[:role]
