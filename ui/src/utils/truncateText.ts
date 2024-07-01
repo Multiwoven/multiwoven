80b4581384effcd6a27542c349889fdfc979e9c2
Sumit Dhanania <sumitdhanania94@gmail.com>
2024-07-01 21:53:27 +0530

    Resolve conflict in cherry-pick of d9efd7fdcf9406b38f0303401af2a5ae0b71521f and change the commit message

---
+++ ui/src/utils/truncateText.ts
@@ -0,0 +1,6 @@
+const truncateText = (text: string, maxLength: number) => {
+  if (text.length <= maxLength) return text;
+  return text.substring(0, maxLength) + '...';
+};
+
+export default truncateText;
