c4a51f3ec7cfe76178fa96514d6f7fc8f95fc639
afthab vp <afthab@multiwoven.com>
2024-08-12 16:07:00 +0530

    Resolve conflict in cherry-pick of 27c4c0e45853349f46179435d764cbd7942e44d1 and change the commit message

--- integrations/lib/multiwoven/integrations/rollout.rb
+++ integrations/lib/multiwoven/integrations/rollout.rb
@@ -2,7 +2,11 @@
 
 module Multiwoven
   module Integrations
+<<<<<<< HEAD
     VERSION = "0.7.9"
+=======
+    VERSION = "0.8.1"
+>>>>>>> 27c4c0e4 (chore(CE): fix oci8 version (#375))
 
     ENABLED_SOURCES = %w[
       Snowflake
