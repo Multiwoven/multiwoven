96f794097125167996a11b6d7544c2a264726bcd
Sumit Dhanania <sumitdhanania@Sumits-MacBook-Pro-2.local>
2024-08-20 22:50:24 +0530

    chore(CE): resolve merge conflicts

--- ui/src/hooks/useErrorToast.tsx
+++ ui/src/hooks/useErrorToast.tsx
@@ -1,23 +1,11 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import useCustomToast from '@/hooks/useCustomToast';
 import { CustomToastStatus } from '@/components/Toast';
 import { ErrorResponse } from '@/services/common';
 
-export const useErrorToast = (isError: boolean, data: any, isFetched: boolean, message: string) => {
+export const useErrorToast = () => {
   const showToast = useCustomToast();
 
-<<<<<<< HEAD
-  useEffect(() => {
-    if (isError || (!data && isFetched)) {
-      showToast({
-        title: `Error: ${message}`,
-        description: message,
-        status: CustomToastStatus.Error,
-        position: 'bottom-right',
-      });
-    }
-  }, [isError, data, isFetched, showToast, message]);
-=======
   const showErrorToast = useCallback(
     (message: string, isError: boolean, data: any, isFetched: boolean) => {
       if (isError && !data && isFetched) {
@@ -33,7 +21,6 @@ export const useErrorToast = (isError: boolean, data: any, isFetched: boolean, m
   );
 
   return showErrorToast;
->>>>>>> d901be2e (refactor(CE): changed condition to render toast)
 };
 
 export const useAPIErrorsToast = () => {
