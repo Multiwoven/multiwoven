6aabe7087d69f74e749ebf350ee57195437e804f
Tushar Selvakumar <54372016+macintushar@users.noreply.github.com>
2024-08-20 16:40:08 +0530

    Resolve conflict in cherry-pick of 5038b91cec902ebd6a23354d259478c3db831967 and change the commit message

--- ui/src/routes/main.tsx
+++ ui/src/routes/main.tsx
@@ -1,9 +1,25 @@
 import { Suspense, lazy } from 'react';
 const AboutUs = lazy(() => import('@/views/AboutUs'));
 const Dashboard = lazy(() => import('@/views/Dashboard'));
+<<<<<<< HEAD
 const SignIn = lazy(() => import('@/views/Authentication/SignIn'));
 const SignUp = lazy(() => import('@/views/Authentication/SignUp'));
 const Models = lazy(() => import('@/views/Models'));
+=======
+
+const SignIn = lazy(() => import('@/enterprise/views/Authentication/SignIn'));
+const SignUp = lazy(() => import('@/enterprise/views/Authentication/SignUp'));
+
+const SignUpVerification = lazy(
+  () => import('@/enterprise/views/Authentication/SignUp/SignUpVerification'),
+);
+const VerifyUser = lazy(() => import('@/enterprise/views/Authentication/VerifyUser'));
+
+const ForgotPassword = lazy(() => import('@/enterprise/views/Authentication/ForgotPassword'));
+const ResetPassword = lazy(() => import('@/enterprise/views/Authentication/ResetPassword'));
+
+const SetupDefine = lazy(() => import('@/views/Define/SetupDefine'));
+>>>>>>> 5038b91c (refactor(CE): changed setup models to setup define)
 const SetupConnectors = lazy(() => import('@/views/Connectors/SetupConnectors'));
 
 const SetupActivate = lazy(() => import('@/views/Activate/SetupActivate'));
@@ -46,7 +62,13 @@ export const MAIN_PAGE_ROUTES: MAIN_PAGE_ROUTES_ITEM[] = [
     url: '/define/*',
     component: (
       <SuspenseWithLoader redirectRoute='/define'>
+<<<<<<< HEAD
         <Models />
+=======
+        <RoleAccess location='model' type='page' action={UserActions.Read}>
+          <SetupDefine />
+        </RoleAccess>
+>>>>>>> 5038b91c (refactor(CE): changed setup models to setup define)
       </SuspenseWithLoader>
     ),
   },
