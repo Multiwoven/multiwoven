62708a742d09985e13dafd695596832c9ba20e0a
datafloyd <subinthattaparambil@gmail.com>
2024-08-23 17:10:43 +0400

    feat(CE): add AI/ML query type to model  (#399)

--- server/app/serializers/model_serializer.rb
+++ server/app/serializers/model_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ModelSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :query, :query_type, :primary_key, :created_at, :updated_at
+  attributes :id, :name, :description, :query, :query_type, :configuration, :primary_key, :created_at, :updated_at
 
   attribute :connector do
     ConnectorSerializer.new(object.connector).attributes
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_26_095056) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_23_114746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_26_095056) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.jsonb "configuration"
   end
 
   create_table "organizations", force: :cascade do |t|
