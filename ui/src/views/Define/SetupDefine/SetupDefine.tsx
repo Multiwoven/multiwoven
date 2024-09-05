76ac05b2c526a743df2bbef55f955365e0ca5293
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-09-06 00:47:14 +0530

    refactor(CE): added setup define component

--- ui/src/views/Define/SetupDefine/SetupDefine.tsx
+++ ui/src/views/Define/SetupDefine/SetupDefine.tsx
@@ -1,17 +1,8 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD:ui/src/views/Models/SetupModels.tsx
-import ModelsList from './ModelsList';
-import ModelsForm from './ModelsForm';
-import ViewModel from './ViewModel';
-import EditModel from './EditModel';
-=======
 import ModelsList from '@/views/Models/ModelsList';
 import ModelsForm from '@/views/Models/ModelsForm';
 import ViewModel from '@/views/Models/ViewModel';
 import EditModel from '@/views/Models/EditModel';
-import RoleAccess from '@/enterprise/components/RoleAccess';
-import { UserActions } from '@/enterprise/types';
->>>>>>> 5038b91c (refactor(CE): changed setup models to setup define):ui/src/views/Define/SetupDefine/SetupDefine.tsx
 
 const SetupModels = (): JSX.Element => {
   return (
