1be6781d4d4e0f4d57a52bc51a9561698b9b64b4
afthab vp <afthab01@gmail.com>
2024-10-01 18:43:35 +0530

    fix(CE): merge conficts

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_08_29_131951) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_09_23_150740) do
->>>>>>> cf7f9380 (chore(CE): add data_app_token with unique constraint (#452))
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
