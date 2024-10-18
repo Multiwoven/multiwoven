d9faebb3bf8c104d2d45be809bf0c6e90c575219
afthab vp <afthab01@gmail.com>
2024-10-18 10:51:25 +0530

    fix(CE): resolve conflicts

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_10_10_214745) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_10_17_114319) do
->>>>>>> 29fbdaaf (chore(CE): feedback model changes (#490))
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
