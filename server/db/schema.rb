94e5e8b5bfc9b7bd56a69e13c30914b7752baadf
afthab vp <afthab@multiwoven.com>
2024-11-07 15:07:06 +0530

    chore(CE): add additional_remarks in feedback model (#554)

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_24_103527) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_07_074907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_24_103527) do
     t.datetime "updated_at", null: false
     t.integer "feedback_type", default: 0, null: false
     t.string "session_id"
+    t.jsonb "additional_remarks"
   end
 
   create_table "models", force: :cascade do |t|
