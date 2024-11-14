91ef7b4fae83ac217124664b259dd40f694a82a0
afthab vp <afthab01@gmail.com>
2024-11-14 11:18:34 +0530

    chore(CE): revert health checker

--- server/lib/utils/health_checker.rb
+++ server/lib/utils/health_checker.rb
@@ -1,47 +1,20 @@
-# frozen_string_literal: true
-
-require "puma"
-require "rack"
-
 module Utils
   class HealthChecker
-    HEALTH_CHECK_HOST = ENV["MULTIWOVEN_WORKER_HEALTH_CHECK_HOST"] || "127.0.0.1"
-    HEALTH_CHECK_PORT = ENV["MULTIWOVEN_WORKER_HEALTH_CHECK_PORT"] || 4567
-
-    @server = nil
-
     def self.run
-      app = proc do |env|
-        if env["PATH_INFO"] == "/health"
-          [200, { "Content-Type" => "text/plain" }, ["Service is healthy"]]
-        else
-          [404, { "Content-Type" => "text/plain" }, ["Not Found"]]
-        end
+      health_check_port = ENV["MULTIWOVEN_WORKER_HEALTH_CHECK_PORT"] || 4567
+
+      server = WEBrick::HTTPServer.new(Port: health_check_port)
+      server.mount_proc "/health" do |_req, res|
+        res.body = "Service is healthy"
       end
 
-      @server = Puma::Server.new(app)
-      @server.add_tcp_listener(HEALTH_CHECK_HOST, HEALTH_CHECK_PORT.to_i)
+      trap "INT" do
+        server.shutdown
+      end
 
-      # Start the server in a new thread
       Thread.new do
-        puts "Starting Puma health check server on #{HEALTH_CHECK_HOST}:#{HEALTH_CHECK_PORT}"
-        @server.run
+        server.start
       end
-
-      # Handle signals for graceful shutdown
-      %w[INT TERM].each do |signal|
-        trap(signal) do
-          puts "Received #{signal}, shutting down Puma health check server..."
-          shutdown
-        end
-      end
-    end
-
-    def self.shutdown
-      return unless @server
-
-      @server.stop(true) # Graceful shutdown
-      puts "Puma health check server stopped."
     end
   end
 end
--- server/spec/lib/utils/health_checker_spec.rb
+++ server/spec/lib/utils/health_checker_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe Utils::HealthChecker do
   describe ".run" do
-    let(:port) { ENV["MULTIWOVEN_WORKER_HEALTH_CHECK_PORT"] || 4567 } # Using a non-standard port to avoid conflicts
+    let(:port) { 9999 } # Using a non-standard port to avoid conflicts
 
     before do
       ENV["MULTIWOVEN_WORKER_HEALTH_CHECK_PORT"] = port.to_s
@@ -19,8 +19,7 @@ RSpec.describe Utils::HealthChecker do
     end
 
     it "responds to /health with a success message" do
-      host = ENV["MULTIWOVEN_WORKER_HEALTH_CHECK_HOST"] || "127.0.0.1"
-      response = Net::HTTP.get_response(URI("http://#{host}:#{port}/health"))
+      response = Net::HTTP.get_response(URI("http://localhost:#{port}/health"))
       expect(response.body).to eq("Service is healthy")
       expect(response.code).to eq("200")
     end
