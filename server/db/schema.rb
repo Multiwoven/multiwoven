bdf5c32c5db110024f83017449028db395c7e390
afthab vp <afthab@multiwoven.com>
2024-11-26 19:29:16 +0530

    chore(CE): model changes for file upload (#631)

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_19_210518) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_26_104103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "audit_logs", force: :cascade do |t|
     t.integer "user_id"
@@ -46,6 +74,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_210518) do
     t.string "connector_name"
     t.string "description"
     t.string "connector_category", default: "data", null: false
+  end
+
+  create_table "custom_visual_component_files", force: :cascade do |t|
+    t.string "file_name"
+    t.integer "workspace_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "data_app_sessions", force: :cascade do |t|
@@ -265,6 +300,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_210518) do
     t.index ["organization_id"], name: "index_workspaces_on_organization_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "workspace_users", "roles"
   add_foreign_key "workspace_users", "users"
   add_foreign_key "workspace_users", "workspaces", on_delete: :nullify
--- server/spec/models/workspace_spec.rb
+++ server/spec/models/workspace_spec.rb
@@ -32,6 +32,7 @@ RSpec.describe Workspace, type: :model do
     it { should have_many(:data_apps).dependent(:nullify) }
     it { should have_many(:data_app_sessions).dependent(:nullify) }
     it { should have_many(:audit_logs).dependent(:nullify) }
+    it { should have_many(:custom_visual_component_files).dependent(:nullify) }
     it { should belong_to(:organization) }
   end
 
