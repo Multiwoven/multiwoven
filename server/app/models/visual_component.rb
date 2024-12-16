16e72067f06048dded4cbaf10b9f9d0b880377e6
afthab vp <afthab@multiwoven.com>
2024-12-16 19:04:21 +0530

    chore(CE): Add chatbot component_type to vc (#722)

--- server/app/models/visual_component.rb
+++ server/app/models/visual_component.rb
@@ -6,7 +6,7 @@ class VisualComponent < ApplicationRecord
   validates :model_id, presence: true
   validates :data_app_id, presence: true
 
-  enum component_type: { doughnut: 0, bar: 1, data_table: 2, visual_text: 3, custom: 4 }
+  enum component_type: { doughnut: 0, bar: 1, data_table: 2, visual_text: 3, custom: 4, chat_bot: 5 }
 
   belongs_to :workspace
   belongs_to :data_app
