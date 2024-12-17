8e8fd254d50653fe6f54a767ff8451c038c0a722
TivonB-AI2 <124182151+TivonB-AI2@users.noreply.github.com>
2024-12-17 09:10:28 -0500

    chore(CE): Add resource link to audit logs serializer (#726)

--- server/app/serializers/audit_logs_serializer.rb
+++ server/app/serializers/audit_logs_serializer.rb
@@ -2,8 +2,8 @@
 
 # app/serializers/audit_logs_serializer.rb
 class AuditLogsSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :user_name, :action, :resource_type, :resource_id, :resource, :workspace_id,
-             :metadata, :created_at, :updated_at
+  attributes :id, :user_id, :user_name, :action, :resource_type, :resource_id, :resource, :resource_link,
+             :workspace_id, :metadata, :created_at, :updated_at
 
   def user_name
     object.user&.name
