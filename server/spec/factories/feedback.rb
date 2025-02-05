5c2efeaeaf82498e06f9c63b616e0ac95a050130
TivonB-AI2 <124182151+TivonB-AI2@users.noreply.github.com>
2025-02-05 11:16:20 -0500

    Resolve conflict in cherry-pick of 0c8585c29890aee96fada888ba4c914e32834074 and change the commit message

--- server/spec/factories/feedback.rb
+++ server/spec/factories/feedback.rb
@@ -9,5 +9,11 @@ FactoryBot.define do
     session_id { SecureRandom.hex(10) }
     feedback_type { "thumbs" }
     reaction { "positive" }
+<<<<<<< HEAD:server/spec/factories/feedback.rb
+=======
+    chatbot_interaction { { user_query: "Hello", chatbot_reply: "Hi! How can I assist you!" } }
+
+    created_at { Time.zone.now }
+>>>>>>> 0c8585c2 (chore(CE): Update Message Feedback Factory (#835)):server/spec/factories/message_feedbacks.rb
   end
 end
