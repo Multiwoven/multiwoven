50d42c9baeba45e3270eabe389814ffe4b018a9d
datafloyd <subinthattaparambil@gmail.com>
2025-02-10 01:22:40 +0400

    fix(CE):  data migrate conflict

--- server/db/data_schema.rb
+++ server/db/data_schema.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
-
-DataMigrate::Data.define(version: 20_250_114_120_956)
-=======
 DataMigrate::Data.define(version: 20_250_206_101_336)
->>>>>>> 32418d44 (feat(CE): add billing models (#833))
