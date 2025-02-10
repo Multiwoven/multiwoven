7f2504a8be3ff4861f9ae42b0e9fa85177827a61
TivonB-AI2 <tivon.brown@squared.ai>
2025-02-10 08:35:58 -0500

    chore(CE): Update

--- server/spec/factories/feedback.rb
+++ server/spec/factories/feedback.rb
@@ -2,23 +2,12 @@
 
 FactoryBot.define do
   factory :feedback do
-    data_app { create(:data_app) }
-    visual_component { create(:visual_component) }
-    model { create(:model) }
+    association :data_app
+    association :visual_component
     association :workspace
+    association :model
     session_id { SecureRandom.hex(10) }
     feedback_type { "thumbs" }
     reaction { "positive" }
-<<<<<<< HEAD
-<<<<<<< HEAD:server/spec/factories/feedback.rb
-=======
-    chatbot_interaction { { user_query: "Hello", chatbot_reply: "Hi! How can I assist you!" } }
-
-    created_at { Time.zone.now }
->>>>>>> 0c8585c2 (chore(CE): Update Message Feedback Factory (#835)):server/spec/factories/message_feedbacks.rb
-=======
-
-    created_at { Time.zone.now }
->>>>>>> main
   end
 end
