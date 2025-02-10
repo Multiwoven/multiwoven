fca5f94a09c9c5d17f10a7746d605d734e65966e
TivonB-AI2 <tivon.brown@squared.ai>
2025-02-10 08:55:36 -0500

    chore(CE): Update

--- server/spec/factories/feedback.rb
+++ server/spec/factories/feedback.rb
@@ -2,12 +2,14 @@
 
 FactoryBot.define do
   factory :feedback do
-    association :data_app
-    association :visual_component
+    data_app { create(:data_app) }
+    visual_component { create(:visual_component) }
+    model { create(:model) }
     association :workspace
-    association :model
     session_id { SecureRandom.hex(10) }
     feedback_type { "thumbs" }
     reaction { "positive" }
+
+    created_at { Time.zone.now }
   end
 end
