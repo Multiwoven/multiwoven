deef02bf5f3b2183e2df2981050ba7b221865f32
TivonB-AI2 <tivon.brown@squared.ai>
2025-02-21 09:35:15 -0500

    chore(CE): Resolve schema conflicts

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2025_02_11_130824) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2025_02_20_021540) do
->>>>>>> 2e8bc4f4 (chore(CE): add SSO Configuration to DB (#854))
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
