fe7ab8844a89fa295451ccf98b752dc2abbf3f0c
datafloyd <subinthattaparambil@gmail.com>
2025-02-25 15:49:22 +0400

    fix(CE): resolve conflicts

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2025_02_11_180054) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2025_02_20_191433) do
->>>>>>> 86d4c3b0 (chore(CE): add role type to role (#868))
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
