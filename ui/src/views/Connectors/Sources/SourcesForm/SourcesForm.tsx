f4c0bffdd4171949de29e9a7b5324a554ed8c002
pabss-ai2 <125898021+pabss-ai2@users.noreply.github.com>
2025-02-25 13:40:13 -0400

    Resolve conflict in cherry-pick of 6e1cfad30a8693918079a701e55f3a086ba6e6a5 and change the commit message

--- ui/src/views/Connectors/Sources/SourcesForm/SourcesForm.tsx
+++ ui/src/views/Connectors/Sources/SourcesForm/SourcesForm.tsx
@@ -1,6 +1,6 @@
-import SteppedForm from '@/components/SteppedForm';
 import SelectDataSourcesForm from './SelectDataSourcesForm';
 
+<<<<<<< HEAD
 import { Box, Drawer, DrawerBody, DrawerContent, DrawerOverlay } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import ConnectorConfigForm from '@/views/Connectors/ConnectorConfigForm';
@@ -9,6 +9,19 @@ import ConnectorFinaliseForm from '@/views/Connectors/ConnectorFinaliseForm';
 
 const SourcesForm = (): JSX.Element => {
   const navigate = useNavigate();
+=======
+import { useParams } from 'react-router-dom';
+import ConnectorConfigForm from '@/views/Connectors/ConnectorConfigForm';
+import ConnectorConnectionTest from '@/views/Connectors/ConnectorConnectionTest';
+import ConnectorFinaliseForm from '@/views/Connectors/ConnectorFinaliseForm';
+import { AIMLSourceFormSteps } from '@/enterprise/views/AIMLSources/SourceFormSteps/SourceFormSteps';
+import { SourceTypes } from '../../types';
+import SteppedFormDrawer from '@/components/SteppedFormDrawer';
+
+const SourcesForm = (): JSX.Element => {
+  const { sourceType } = useParams();
+
+>>>>>>> 6e1cfad3 (fix(CE): Content centered at max width)
   const steps = [
     {
       formKey: 'datasource',
@@ -37,6 +50,7 @@ const SourcesForm = (): JSX.Element => {
   ];
 
   return (
+<<<<<<< HEAD
     <Drawer isOpen onClose={() => navigate(-1)} placement='right' size='100%'>
       <DrawerOverlay />
       <DrawerContent padding='0px'>
@@ -47,6 +61,9 @@ const SourcesForm = (): JSX.Element => {
         </DrawerBody>
       </DrawerContent>
     </Drawer>
+=======
+    <SteppedFormDrawer steps={sourceType === SourceTypes.AI_ML ? AIMLSourceFormSteps : steps} />
+>>>>>>> 6e1cfad3 (fix(CE): Content centered at max width)
   );
 };
 
