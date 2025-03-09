6ef3d259e554c7d4ca1a109200b3e53fea1ae35f
afthab vp <afthab@multiwoven.com>
2025-03-09 17:49:54 +0530

    fix(CE): active storage to local (#638)

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :microsoft
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
