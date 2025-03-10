e658c2d04a7bfe585dc9e34e7d5fe0644a1cf2fe
TivonB-AI2 <124182151+TivonB-AI2@users.noreply.github.com>
2025-03-10 04:44:41 -0400

    Resolve conflict in cherry-pick of cd1bbac5fb948b97a07d551dd502c580643b8240 and change the commit message

--- server/app/controllers/api/v1/workspaces_controller.rb
+++ server/app/controllers/api/v1/workspaces_controller.rb
@@ -5,7 +5,10 @@ module Api
   module V1
     class WorkspacesController < ApplicationController
       include Workspaces
+      include AuditLogger
+      include ResourceLinkBuilder
       skip_after_action :verify_authorized, only: %i[index show]
+      after_action :create_audit_log, only: %i[create update]
 
       def index
         result = ListAll.call(user: current_user)
@@ -31,6 +34,13 @@ module Api
         result = Create.call(user: current_user, workspace_params:)
         if result.success?
           @workspace = result.workspace
+<<<<<<< HEAD
+=======
+          @audit_resource = @workspace.name
+          @resource_id = @workspace.id
+          @payload = workspace_params
+          authorize @workspace
+>>>>>>> cd1bbac5 (chore(CE): Add Audit to Workspace Controller (#913))
           render json: result.workspace, status: :created
         else
           render_error(
@@ -46,6 +56,12 @@ module Api
         result = Update.call(id: params[:id], user: current_user, workspace_params:)
         if result.success?
           @workspace = result.workspace
+<<<<<<< HEAD
+=======
+          @audit_resource = @workspace.name
+          @payload = workspace_params
+          authorize @workspace
+>>>>>>> cd1bbac5 (chore(CE): Add Audit to Workspace Controller (#913))
           render json: @workspace, status: :ok
         else
           render_error(
@@ -58,6 +74,9 @@ module Api
 
       def destroy
         authorize current_workspace, policy_class: WorkspacePolicy
+        action = "delete"
+        resource = current_user.workspaces.find_by(id: params[:id]).name
+        audit!(action:, resource_id: params[:id], resource:)
         result = Workspaces::Delete.call(id: params[:id], user: current_user)
         if result.success?
           head :no_content
@@ -71,6 +90,12 @@ module Api
       end
 
       private
+
+      def create_audit_log
+        resource_id = @resource_id || params[:id]
+        resource_link = build_link!(resource_id:)
+        audit!(action: @action, resource_id:, resource: @audit_resource, payload: @payload, resource_link:)
+      end
 
       def workspace_params
         params.require(:workspace).permit(:name, :organization_id, :description, :region)
