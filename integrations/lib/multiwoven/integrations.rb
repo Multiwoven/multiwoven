65a2602cc31ae2298abd16443543e5b50dbf4652
TivonB-AI2 <124182151+TivonB-AI2@users.noreply.github.com>
2025-03-17 16:05:46 -0400

    feat(CE): Add WatsonX.Ai Connector (#920)

--- integrations/lib/multiwoven/integrations.rb
+++ integrations/lib/multiwoven/integrations.rb
@@ -75,6 +75,7 @@ require_relative "integrations/source/google_vertex_model/client"
 require_relative "integrations/source/http_model/client"
 require_relative "integrations/source/open_ai/client"
 require_relative "integrations/source/sftp/client"
+require_relative "integrations/source/watsonx_ai/client"
 
 # Destination
 require_relative "integrations/destination/klaviyo/client"
