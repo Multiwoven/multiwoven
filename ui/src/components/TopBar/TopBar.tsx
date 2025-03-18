13b82abb714d1eff57d645fc32e29f49bdf1a93f
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-03-18 21:17:38 +0530

    Resolve conflict in cherry-pick of d2c965ab5a257c86ee95d9d3aecd36129ad98d16 and change the commit message

--- ui/src/components/TopBar/TopBar.tsx
+++ ui/src/components/TopBar/TopBar.tsx
@@ -1,9 +1,12 @@
-import { Box, Button, Flex, HStack, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Heading, Icon, Text, Tooltip } from '@chakra-ui/react';
 import { Step } from '../Breadcrumbs/types';
 import Breadcrumbs from '../Breadcrumbs';
+import { FiInfo } from 'react-icons/fi';
 
 type TopBarType = {
   name: string;
+  nameTooltip?: string;
+  nameTooltipVisible?: boolean;
   ctaName?: string;
   ctaIcon?: JSX.Element;
   ctaColor?: string;
@@ -20,6 +23,8 @@ type TopBarType = {
 
 const TopBar = ({
   name,
+  nameTooltip = '',
+  nameTooltipVisible = false,
   ctaName = '',
   ctaIcon,
   onCtaClicked = () => {},
@@ -29,6 +34,7 @@ const TopBar = ({
   ctaButtonWidth,
   ctaButtonHeight,
   breadcrumbSteps = [],
+<<<<<<< HEAD
 }: TopBarType): JSX.Element => (
   <Flex justifyContent='space-between' borderColor='gray.400' marginBottom='30px'>
     <Box>
@@ -56,5 +62,57 @@ const TopBar = ({
     </HStack>
   </Flex>
 );
+=======
+}: TopBarType): JSX.Element => {
+  return (
+    <Flex justifyContent='space-between' borderColor='gray.400' marginBottom='30px'>
+      <Box>
+        {breadcrumbSteps.length > 0 ? <Breadcrumbs steps={breadcrumbSteps} /> : null}
+        <Box display='flex' alignItems='center' gap='4px'>
+          <Heading fontWeight='bold' size='sm'>
+            {name}
+          </Heading>
+          {nameTooltipVisible && nameTooltip.length > 0 && (
+            <Box h='32px' w='32px' display='flex' alignItems='center' justifyContent='center'>
+              <Tooltip
+                hasArrow
+                label={nameTooltip}
+                fontSize='xs'
+                placement='bottom-start'
+                backgroundColor='black.500'
+                color='gray.100'
+                borderRadius='6px'
+                padding='8px'
+                width='auto'
+              >
+                <span>
+                  <Icon as={FiInfo} color='gray.600' w='16px' h='16px' />
+                </span>
+              </Tooltip>
+            </Box>
+          )}
+        </Box>
+      </Box>
+      <HStack spacing={2} style={{ alignItems: 'flex-end' }}>
+        {extra}
+        {isCtaVisible ? (
+          <Button
+            variant={ctaButtonVariant}
+            leftIcon={ctaIcon}
+            width={ctaButtonWidth || '126px'}
+            height={ctaButtonHeight || '40px'}
+            onClick={onCtaClicked}
+            fontSize='16px'
+          >
+            <Text size='sm'>{ctaName}</Text>
+          </Button>
+        ) : (
+          <></>
+        )}
+      </HStack>
+    </Flex>
+  );
+};
+>>>>>>> d2c965ab (feat(CE): added tooltip to titlebar)
 
 export default TopBar;
