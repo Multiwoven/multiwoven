da4e17c6b4477d99aa3230cdc0bec261597fab20
afthab vp <afthab@multiwoven.com>
2025-04-14 18:42:11 +0530

    Resolve conflict in cherry-pick of e42a63cf977501f50cc9d5ed2b9f03fe4cfa5018 and change the commit message

--- server/db/data_schema.rb
+++ server/db/data_schema.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
+<<<<<<< HEAD
 DataMigrate::Data.define(version: 20_250_403_013_305)
+=======
+DataMigrate::Data.define(version: 20_250_414_125_119)
+>>>>>>> e42a63cf (chore(CE): add assistant eula sso resources in model (#1015))
