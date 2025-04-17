147aef661de2545bdab6a36bd316abaabb9c5bb4
afthab vp <afthab01@gmail.com>
2025-04-17 21:39:26 +0530

    fix(CE): migration

--- server/db/data_schema.rb
+++ server/db/data_schema.rb
@@ -1,7 +1,3 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
-DataMigrate::Data.define(version: 20_250_403_013_305)
-=======
 DataMigrate::Data.define(version: 20_250_414_125_119)
->>>>>>> e42a63cf (chore(CE): add assistant eula sso resources in model (#1015))
