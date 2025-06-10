34b90b14dadd6893217afd0ed432e174909a8674
Rafael E. O'Neill <106079170+RafaelOAiSquared@users.noreply.github.com>
2025-06-10 11:50:47 -0400

    refactor(CE): Update SignUp for email_verification (#759)
    Co-authored-by: TivonB-AI2 <124182151+TivonB-AI2@users.noreply.github.com>

--- server/app/interactors/authentication/signup.rb
+++ server/app/interactors/authentication/signup.rb
@@ -10,8 +10,12 @@ module Authentication
         create_organization_and_workspace
         save_user
         if user.persisted?
-          user.send_confirmation_instructions if User.email_verification_enabled?
-          context.message = "Signup successful! Please check your email to confirm your account."
+          if User.email_verification_enabled?
+            user.send_confirmation_instructions
+            context.message = "Signup successful! Please check your email to confirm your account."
+          else
+            context.message = "Signup successful!"
+          end
         else
           context.fail!(errors: user.errors.full_messages)
         end
