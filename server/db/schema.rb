9fc93d84955b651c092c8fdc459f50492b4af087
afthab vp <afthab01@gmail.com>
2025-06-23 14:47:57 +0400

    chore(CE): update

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_06_02_100811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
