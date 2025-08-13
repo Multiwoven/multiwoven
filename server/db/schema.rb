4d21b30f2c1d5a119322b4ab715ae797a5e30892
Rafael E. O'Neill <106079170+RafaelOAiSquared@users.noreply.github.com>
2025-08-13 12:00:00 -0400

    chore(CE): Revert adding template to workflow in DB (#812)
    Co-authored-by: TivonB-AI2 <tivon.brown@squared.ai>

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_15_171640) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_17_004726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
