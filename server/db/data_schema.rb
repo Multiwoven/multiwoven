ac09b975f0717411b3bdda91dd0307539d6a1e45
TivonB-AI2 <tivon.brown@squared.ai>
2025-08-13 11:00:33 -0400

    chore(CE): Resolve conflict

--- server/db/data_schema.rb
+++ server/db/data_schema.rb
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
-DataMigrate::Data.define(version: 20250617163641)
-=======
 DataMigrate::Data.define(version: 20250710183353)
->>>>>>> 9eeaeb7f (chore(CE): Migrate existing pg vector connectors to  vector sub_category (#1229))
