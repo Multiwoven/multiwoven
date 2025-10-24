2956cf196a8e56d859fc0827679c3be6e27fc114
TivonB-AI2 <124182151+TivonB-AI2@users.noreply.github.com>
2025-10-24 12:01:35 -0400

    Resolve conflict in cherry-pick of b5159fd2ead76ccb5d71c76f5c72349b8f5e2e3a and change the commit message

--- server/db/data_schema.rb
+++ server/db/data_schema.rb
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 # frozen_string_literal: true
 
 DataMigrate::Data.define(version: 20250724110926)
+=======
+DataMigrate::Data.define(version: 20251024153624)
+>>>>>>> b5159fd2 (fix(CE): Update all existing sub categories for Generic Open AI (#1402))
