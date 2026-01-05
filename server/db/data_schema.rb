8f25bbf666081ed6ad6d972ddada35b8e0bd3e65
TivonB-AI2 <tivon.brown@squared.ai>
2026-01-05 10:27:13 -0500

    chore(CE): resolve conflict

--- server/db/data_schema.rb
+++ server/db/data_schema.rb
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-# frozen_string_literal: true
-
-DataMigrate::Data.define(version: 20250724110926)
-=======
 DataMigrate::Data.define(version: 20251024153624)
->>>>>>> b5159fd2 (fix(CE): Update all existing sub categories for Generic Open AI (#1402))
